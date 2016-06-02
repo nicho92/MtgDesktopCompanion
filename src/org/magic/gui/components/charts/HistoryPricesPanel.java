8e0ab36be4152efcaa48fc2b3441e0b517f9982d
nicho92 <nicolas.pihen@gmail.com>
2016-06-02 11:16:01 +0200

    show edition in prices variation

--- src/org/magic/gui/components/charts/HistoryPricesPanel.java
+++ src/org/magic/gui/components/charts/HistoryPricesPanel.java
@@ -11,10 +11,12 @@ import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.XYTextAnnotation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.XYSeries;
+import org.jfree.data.time.TimeSeriesDataItem;
 import org.magic.api.beans.MagicEdition;
 import org.magic.tools.MagicFactory;
 
@@ -28,27 +30,51 @@ public class HistoryPricesPanel extends JPanel{
 		refresh();
 	}
 	
-	
 	private void refresh()
 	{
 		this.removeAll();
-		
-		  TimeSeries series1 = new TimeSeries("value");
+		TimeSeriesCollection dataset = new TimeSeriesCollection();
+		TimeSeries series1 = new TimeSeries("value");
 			for(Date d : map.keySet())
 				series1.add(new Day(d),map.get(d).doubleValue());
 
-		  TimeSeriesCollection dataset = new TimeSeriesCollection();
-		    dataset.addSeries(series1);
 		
+		dataset.addSeries(series1);
 		JFreeChart chart = ChartFactory.createTimeSeriesChart(
-                "Price Variation",
-                "date",
-                "Price",
-                dataset,
-                true,
-                true,
-                false
-                );
+	                "Price Variation",
+	                "Date",
+	                "Price",
+	                dataset,
+	                true,
+	                true,
+	                false
+	                );	
+			
+			
+			
+		try{
+				for(MagicEdition me: MagicFactory.getInstance().getEnabledProviders().get(0).searchSetByCriteria(null, null))
+					{
+						Date d = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(me.getReleaseDate()+" 00:00");
+						TimeSeriesDataItem  item = series1.getDataItem(new Day(d));
+						
+						if(item!=null)
+						{ 
+							
+						  double x = item.getPeriod().getFirstMillisecond();
+						  double y = item.getValue().doubleValue();
+						  XYTextAnnotation  annot = new XYTextAnnotation (me.getId(),x,y);
+						  
+						  XYPlot plot = (XYPlot) chart.getPlot();
+						  plot.addAnnotation(annot);
+						}
+					}
+				}
+			catch(Exception e)
+			{
+				
+			}
+			
 		
 		pane = new ChartPanel(chart);
 		pane.addMouseWheelListener(new MouseWheelListener() {
