93b6ad5b949bf347417d5734dc4ca02513225ebd
nicho92 <nicolas.pihen@gmail.com>
2016-09-25 13:26:19 +0200

    begin ws server

--- src/org/magic/server/MTGDesktopCompanionServer.java
+++ src/org/magic/server/MTGDesktopCompanionServer.java
@@ -1,111 +1,38 @@
 package org.magic.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Map;
 
-public class MTGDesktopCompanionServer
+import fi.iki.elonen.NanoHTTPD;
+
+public class MTGDesktopCompanionServer  extends NanoHTTPD
 {
-    public static void main(String[] args) throws IOException {
-
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(4444);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 4444.");
-            System.exit(1);
-        }
-
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        KnockKnockProtocol kkp = new KnockKnockProtocol();
-
-        outputLine = kkp.processInput(null);
-        out.println(outputLine);
-
-        while ((inputLine = in.readLine()) != null) {
-             outputLine = kkp.processInput(inputLine);
-             out.println(outputLine);
-             if (outputLine.equals("Bye."))
-                break;
-        }
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
+    public MTGDesktopCompanionServer() throws IOException {
+		super(8080);
+		
+	}
+    
+    @Override
+    public void start() throws IOException {
+    	start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
     }
-}
+    
 
-class KnockKnockProtocol {
-    private static final int WAITING = 0;
-    private static final int SENTKNOCKKNOCK = 1;
-    private static final int SENTCLUE = 2;
-    private static final int ANOTHER = 3;
- 
-    private static final int NUMJOKES = 5;
- 
-    private int state = WAITING;
-    private int currentJoke = 0;
- 
-    private String[] clues = { "Turnip", "Little Old Lady", "Atch", "Who", "Who" };
-    private String[] answers = { "Turnip the heat, it's cold in here!",
-                                 "I didn't know you could yodel!",
-                                 "Bless you!",
-                                 "Is there an owl in here?",
-                                 "Is there an echo in here?" };
- 
-    public String processInput(String theInput) {
-        String theOutput = null;
- 
-        if (state == WAITING) {
-            theOutput = "Knock! Knock!";
-            state = SENTKNOCKKNOCK;
-        } else if (state == SENTKNOCKKNOCK) {
-            if (theInput.equalsIgnoreCase("Who's there?")) {
-                theOutput = clues[currentJoke];
-                state = SENTCLUE;
-            } else {
-                theOutput = "You're supposed to say \"Who's there?\"! " +
-                "Try again. Knock! Knock!";
-            }
-        } else if (state == SENTCLUE) {
-            if (theInput.equalsIgnoreCase(clues[currentJoke] + " who?")) {
-                theOutput = answers[currentJoke] + " Want another? (y/n)";
-                state = ANOTHER;
-            } else {
-                theOutput = "You're supposed to say \"" + 
-                clues[currentJoke] + 
-                " who?\"" + 
-                "! Try again. Knock! Knock!";
-                state = SENTKNOCKKNOCK;
-            }
-        } else if (state == ANOTHER) {
-            if (theInput.equalsIgnoreCase("y")) {
-                theOutput = "Knock! Knock!";
-                if (currentJoke == (NUMJOKES - 1))
-                    currentJoke = 0;
-                else
-                    currentJoke++;
-                state = SENTKNOCKKNOCK;
-            } else {
-                theOutput = "Bye.";
-                state = WAITING;
-            }
-        }
-        return theOutput;
+	public static void main(String[] args) throws IOException {
+    		new MTGDesktopCompanionServer().start();	
     }
+    
+	@Override
+	public Response serve(IHTTPSession session) {
+		  String msg = "<html><body><h1>Hello server</h1>\n";
+          Map<String, String> parms = session.getParms();
+          if (parms.get("att") == null) {
+              msg += "<form action='?' method='get'>\n  <p>search: <input type='text' name='name'></p>\n" + "</form>\n";
+          } else {
+              msg += "<p>Hello, " + parms.get("username") + "!</p>";
+          }
+          return newFixedLengthResponse(msg + "</body></html>\n");
+	}
+    
+    
 }
