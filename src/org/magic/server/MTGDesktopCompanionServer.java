5f1f147d32e5f88ee84918c88068ef9b032b5413
nicho92 <nicolas.pihen@gmail.com>
2016-09-25 17:27:43 +0200

    server beta

--- src/org/magic/server/MTGDesktopCompanionServer.java
+++ src/org/magic/server/MTGDesktopCompanionServer.java
@@ -1,7 +1,15 @@
 package org.magic.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.magic.api.beans.MagicCard;
+import org.magic.api.exports.impl.JsonExport;
+import org.magic.services.MagicFactory;
+
+import com.google.gson.Gson;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -19,19 +27,25 @@ public class MTGDesktopCompanionServer  extends NanoHTTPD
     
 
 	public static void main(String[] args) throws IOException {
+		MagicFactory.getInstance().getEnabledProviders().init();
     		new MTGDesktopCompanionServer().start();	
     }
     
 	@Override
 	public Response serve(IHTTPSession session) {
-		  String msg = "<html><body><h1>Hello server</h1>\n";
-          Map<String, String> parms = session.getParms();
-          if (parms.get("att") == null) {
-              msg += "<form action='?' method='get'>\n  <p>search: <input type='text' name='name'></p>\n" + "</form>\n";
-          } else {
-              msg += "<p>Hello, " + parms.get("username") + "!</p>";
-          }
-          return newFixedLengthResponse(msg + "</body></html>\n");
+		  Map<String, List<String>> parms = session.getParameters();
+		  List<MagicCard> list=new ArrayList<MagicCard>();
+		try {
+			list = MagicFactory.getInstance().getEnabledProviders().searchCardByCriteria(parms.get("att").get(0), parms.get("search").get(0), null);
+			Response resp = newFixedLengthResponse(new Gson().toJson(list));
+					 resp.addHeader("Content-Type", "application/json");
+			
+			return resp;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return newFixedLengthResponse("erreur");
+		}
+		 
 	}
     
     
