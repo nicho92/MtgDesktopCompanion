1b265f9d364ca9de0c103cd8fad4db698f2bac66
nicho92 <nicolas.pihen@gmail.com>
2016-09-26 08:49:23 +0200

    bugfix

--- src/org/magic/server/MTGDesktopCompanionServer.java
+++ src/org/magic/server/MTGDesktopCompanionServer.java
@@ -9,6 +9,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicCollection;
+import org.magic.api.beans.MagicEdition;
 import org.magic.api.beans.MagicPrice;
 import org.magic.api.interfaces.MagicPricesProvider;
 import org.magic.services.MagicFactory;
@@ -58,44 +59,48 @@ public class MTGDesktopCompanionServer  extends NanoHTTPD
 		  
 		  switch(session.getUri())
 		  {
-			  case "/collection":  return searchcardColl(session.getParameters().keySet().toArray()[0].toString(), parms.get(session.getParameters().keySet().toArray()[0].toString()).get(0)); 
-			  case "/search": return searchcard(session.getParameters().keySet().toArray()[0].toString(), parms.get(session.getParameters().keySet().toArray()[0].toString()).get(0));
-			  case "/prices" : return searchPrice(session.getParameters().get("name").get(0).toString(),session.getParameters().get("set").get(0).toString());
+			  case "/collection":  return searchcardColl(session); 
+			  case "/search": return searchcard(session);
+			  case "/prices" : return searchPrice(session);
 			  default : return newFixedLengthResponse("Not usable uri")  ;
 		  }
 	}
 	
-    private Response searchPrice(String string, String string2) {
+    private Response searchPrice(IHTTPSession session) {
     	try {
-  		  MagicCard mc = MagicFactory.getInstance().getEnabledProviders().searchCardByCriteria("name", string, null).get(0);
-  		  
-  		  
-  		  
-  		  List<MagicPrice> pricesret = new ArrayList<MagicPrice>();
-  		  for(MagicPricesProvider prices : MagicFactory.getInstance().getEnabledPricers())
-  		  {
-  			pricesret.addAll(prices.getPrice(mc.getEditions().get(0), mc));
-  		  }
-  		  
-  		 JsonObject card = new JsonObject();
- 		  card.add("card", new Gson().toJsonTree(mc));
- 		  card.add("prices", new Gson().toJsonTree(pricesret));
- 		 
-  		  
-  		  Response resp = newFixedLengthResponse(card.toString());
-  		  resp.addHeader("Content-Type", "application/json");
+	    		String att = session.getParameters().get("name").get(0).toString();
+	    		String val = session.getParameters().get("set").get(0).toString();
+	    		
+	    		MagicCard mc = MagicFactory.getInstance().getEnabledProviders().searchCardByCriteria("name", att, null).get(0);
+	    		MagicEdition ed = MagicFactory.getInstance().getEnabledProviders().searchSetByCriteria("id", val).get(0);
+	    		
+	  		  	List<MagicPrice> pricesret = new ArrayList<MagicPrice>();
+	  		
+	  		  	for(MagicPricesProvider prices : MagicFactory.getInstance().getEnabledPricers())
+	  		  		pricesret.addAll(prices.getPrice(ed, mc));
+	  		  
+	  		  	JsonObject card = new JsonObject();
+	  		  			   card.add("card", new Gson().toJsonTree(mc));
+	  		  			   card.add("prices", new Gson().toJsonTree(pricesret));
+	 		  
+	  		  	Response resp = newFixedLengthResponse(card.toString());
+	  		  			 resp.addHeader("Content-Type", "application/json");
+	  		  			 
   		  return resp;
   	  } 
   	  catch (Exception e) 
   	  {
   		  e.printStackTrace();
-  		 return newFixedLengthResponse("Usage : /prices?name=<cardname>&set=<IDSET>");
+  		 return newFixedLengthResponse("Usage : /prices?name=<i>cardname</i>&set=<i>IDSET</i>");
   	  }
 	}
 
-	private Response searchcard(String att,String name)
+	private Response searchcard(IHTTPSession session)
 	{
 	  try {
+		  String att=session.getParameters().keySet().toArray()[0].toString();
+		  String name=session.getParameters().get(session.getParameters().keySet().toArray()[0].toString()).get(0);
+		  
 		  List<MagicCard> list = MagicFactory.getInstance().getEnabledProviders().searchCardByCriteria(att, name, null);
 		  Response resp = newFixedLengthResponse(new Gson().toJson(list));
 		  resp.addHeader("Content-Type", "application/json");
@@ -104,14 +109,19 @@ public class MTGDesktopCompanionServer  extends NanoHTTPD
 	  catch (Exception e) 
 	  {
 		  e.printStackTrace();
-		  return newFixedLengthResponse("Usage : /search?<att>=<value>");
+		  return newFixedLengthResponse("Usage : /search?<i>att</i>=<i>value</i>");
 	  }
 	}
 	
     
-    private Response searchcardColl(String att,String name)
+    private Response searchcardColl(IHTTPSession session)
 	{
+    	
 	  try {
+		  
+		  String att=session.getParameters().keySet().toArray()[0].toString();
+		  String name=session.getParameters().get(session.getParameters().keySet().toArray()[0].toString()).get(0);
+		  
 		  MagicCollection col = new MagicCollection();
 		  col.setName(name);
 		  List<MagicCard> list = MagicFactory.getInstance().getEnabledDAO().getCardsFromCollection(col);
@@ -122,7 +132,7 @@ public class MTGDesktopCompanionServer  extends NanoHTTPD
 	  catch (Exception e) 
 	  {
 		  e.printStackTrace();
-		  return newFixedLengthResponse("Usage : /search?col=<value>");
+		  return newFixedLengthResponse("Usage : /collection?col=<i>value</i>");
 	  }
 	}
     
