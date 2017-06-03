ae884992e7379acceb4acccdf7f1765cec931bd2
nicho92 <nicolas.pihen@gmail.com>
2017-06-03 22:57:16 +0200

    EmbalmAction

---
+++ src/org/magic/game/actions/cards/EmbalmActions.java
@@ -0,0 +1,45 @@
+package org.magic.game.actions.cards;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+
+import org.magic.api.beans.MagicCard;
+import org.magic.game.gui.components.DisplayableCard;
+import org.magic.game.gui.components.GamePanelGUI;
+import org.magic.services.MTGControler;
+
+public class EmbalmActions extends AbstractAction {
+
+			
+			private DisplayableCard card;
+
+			public EmbalmActions(DisplayableCard card) {
+				super("Embalm");
+				putValue(SHORT_DESCRIPTION, "Create a embalmed copy");
+		        putValue(MNEMONIC_KEY, KeyEvent.VK_E);
+		        this.card = card;
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					MagicCard tok = GamePanelGUI.getInstance().getTokenGenerator().generateTokenFor(card.getMagicCard() );
+					DisplayableCard dc = new DisplayableCard( tok, MTGControler.getInstance().getCardsDimension(),true);
+					dc.setMagicCard(tok);
+					GamePanelGUI.getInstance().getPlayer().exileCardFromGraveyard(card.getMagicCard());
+					GamePanelGUI.getInstance().getPanelGrave().remove(card);
+					GamePanelGUI.getInstance().getPanelBattleField().addComponent(dc);
+					GamePanelGUI.getInstance().getPanelBattleField().revalidate();
+					GamePanelGUI.getInstance().getPanelBattleField().repaint();
+					GamePanelGUI.getInstance().getPanelGrave().postTreatment();
+					GamePanelGUI.getInstance().getPlayer().logAction("Embalm "+  card);
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+
+				
+			}
+}
