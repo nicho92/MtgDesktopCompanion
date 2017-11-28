6076ec63645420ba5690a2c7d020fe522354c67d
Pihen <Pihen@UC4459.mairie-boulogne-billancourt.fr>
2017-11-28 17:12:40 +0100

    log4j appender

---
+++ src/org/magic/services/MTGAppender.java
@@ -0,0 +1,30 @@
+package org.magic.services;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+import org.magic.api.main.MtgDesktopCompanion;
+
+public class MTGAppender extends AppenderSkeleton {
+
+	@Override
+	public void close() {
+		
+
+	}
+
+	@Override
+	public boolean requiresLayout() {
+		return true;
+	}
+
+	@Override
+	protected void append(LoggingEvent event) {
+		if(MtgDesktopCompanion.launch!=null)
+			MtgDesktopCompanion.launch.update(null,event.getMessage().toString());
+	}
+
+
+}
