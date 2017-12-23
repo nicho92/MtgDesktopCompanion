9adf5a89f5d37700161469369a1eb2f9760dea93
nicho92 <nicolas.pihen@gmail.com>
2017-12-23 17:53:32 +0100

    scryfall junit test

---
+++ src/test/java/unit/providers/ScryfallTest.java
@@ -0,0 +1,41 @@
+package unit.providers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.asciitable.impl.ASCIITableImpl;
+import org.asciitable.impl.CollectionASCIITableAware;
+import org.asciitable.spec.IASCIITableAware;
+import org.magic.api.beans.MagicCard;
+import org.magic.api.providers.impl.ScryFallProvider;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.magic.api.beans.RetrievableDeck;
+import org.magic.api.decksniffer.impl.TappedOutDeckSniffer;
+
+public class ScryfallTest {
+	
+	ScryFallProvider prov;
+	
+	
+	@Before
+	public void connexion()
+	{
+		prov = new ScryFallProvider();
+		prov.init();
+		
+	}
+	
+	@Test
+	public void testSearch()
+	{
+		try {
+			List<MagicCard> res = prov.searchCardByCriteria("name", "liliana", null);
+		} catch (Exception e) {
+			fail("error" + e);
+		}
+	}
+	
+}
