a19db24b524e5dd62d62342baca21ada94e85027
nicho92 <nicolas.pihen@gmail.com>
2018-01-06 13:59:07 +0100

    use Reflection api for test mtgplugins

--- src/main/java/org/magic/services/ModuleInstaller.java
+++ src/main/java/org/magic/services/ModuleInstaller.java
@@ -2,13 +2,16 @@ package org.magic.services;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.magic.api.interfaces.MTGPlugin;
 import org.magic.api.interfaces.MagicDAO;
+import org.reflections.Reflections;
 
 public class ModuleInstaller {
 
@@ -17,41 +20,16 @@ public class ModuleInstaller {
 	Logger logger = MTGLogger.getLogger(this.getClass());
 	
 	 public List<Class> getClasses(String packageName) throws ClassNotFoundException, IOException {
-	        ClassLoader classLoader = ModuleInstaller.class.getClassLoader();//Thread.currentThread().getContextClassLoader();
-	        String path = packageName.replace('.', '/');
-	        Enumeration<URL> resources = classLoader.getResources(path);
-	        List<File> dirs = new ArrayList<File>();
-	        while (resources.hasMoreElements()) {
-	        	URL resource = resources.nextElement();
-	            dirs.add(new File(resource.getFile().replaceAll("%20", " ")));
-	           
-	        }
-	        ArrayList<Class> classes = new ArrayList<Class>();
-	        for (File directory : dirs) {
-	        	classes.addAll(findClasses(directory, packageName));
-	        }
-	      
-	        return classes;
+		 ArrayList<Class> classes = new ArrayList<Class>();
+		 Reflections reflections = new Reflections(packageName);
+		 for(Class c :reflections.getSubTypesOf(MTGPlugin.class) )
+		 {
+			if(!c.isInterface() && !Modifier.isAbstract(c.getModifiers()))
+				classes.add(c);
+		 }
+	      return classes;
 	    }
 	
-	 
-	 private List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-	        List<Class> classes = new ArrayList<Class>();
-	        if (!directory.exists()) {
-	            return classes;
-	        }
-	        File[] files = directory.listFiles();
-	        
-	        for (File file : files) {
-	        	if (file.isDirectory()) {
-	                assert !file.getName().contains(".");
-	                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-	            } else if (file.getName().endsWith(".class")) {
-	                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-	            }
-	        }
-	        return classes;
-	    }
 	
 	 public boolean updateConfigWithNewModule() throws ClassNotFoundException, IOException {
 		
