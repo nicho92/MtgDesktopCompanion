97ab97d4eba763b67ee39425248cc077b7c6d76a
nicho92 <nicolas.pihen@gmail.com>
2018-01-20 14:27:21 +0100

    BestTrendingDashlet now filter on game formats

--- src/main/java/org/magic/gui/dashlet/BestTrendingDashlet.java
+++ src/main/java/org/magic/gui/dashlet/BestTrendingDashlet.java
@@ -2,13 +2,18 @@ package org.magic.gui.dashlet;
 
 import java.awt.BorderLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
@@ -33,10 +38,15 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 	private CardsShakerTableModel modStandard;
 	private JSpinner spinner;
 	
+	private JCheckBox boxS;
+	private JCheckBox boxM;
+	private JCheckBox boxV;
+	private JCheckBox boxL;
+	
+	
 	public BestTrendingDashlet() {
 		super();
 		setFrameIcon(MTGConstants.ICON_UP);
-		
 	}
 	
 	@Override
@@ -56,9 +66,16 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 				
 				try {
 					List<CardShake> shakes = new ArrayList<CardShake>();
-							shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.modern.toString()));
-						//	shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.standard.toString()));
 							
+					if(boxM.isSelected())
+						shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.modern.toString()));
+					if(boxS.isSelected())	
+						shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.standard.toString()));
+					if(boxL.isSelected())		
+						shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.legacy.toString()));
+					if(boxV.isSelected())		
+						shakes.addAll(MTGControler.getInstance().getEnabledDashBoard().getShakerFor(FORMAT.vintage.toString()));
+					
 					
 					Collections.sort(shakes,new Comparator<CardShake>() {
 						public int compare(CardShake o1, CardShake o2) {
@@ -75,6 +92,12 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 					
 					int val = (Integer)spinner.getValue(); 
 					save("LIMIT", String.valueOf(val));
+					save("STD",String.valueOf(boxS.isSelected()));
+					save("MDN",String.valueOf(boxM.isSelected()));
+					save("LEG",String.valueOf(boxL.isSelected()));
+					save("VIN",String.valueOf(boxV.isSelected()));
+					
+					
 					List<CardShake> ret = new ArrayList<CardShake>();
 					ret.addAll(shakes.subList(0, val));//X first
 					ret.addAll(shakes.subList(shakes.size()-(val+1), shakes.size())); //x last
@@ -100,6 +123,28 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 	public void initGUI() {
 		JPanel panneauHaut = new JPanel();
 		getContentPane().add(panneauHaut, BorderLayout.NORTH);
+
+		Action a = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				init();
+				
+			}
+		};
+
+		
+		boxS= new JCheckBox();
+		boxS.setAction(a);
+		boxS.setText("STD");
+		boxM= new JCheckBox();
+		boxM.setAction(a);
+		boxM.setText("MDN");
+		boxL= new JCheckBox();
+		boxL.setAction(a);
+		boxL.setText("LEG");
+		boxV= new JCheckBox("V");
+		boxV.setAction(a);
+		boxV.setText("VIN");
 		
 		spinner = new JSpinner();
 		spinner.addChangeListener(new ChangeListener() {
@@ -107,9 +152,16 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 				init();
 			}
 		});
-		spinner.setModel(new SpinnerNumberModel(new Integer(5), new Integer(1), null, new Integer(1)));
-		panneauHaut.add(spinner);
 		
+		
+		
+		
+		spinner.setModel(new SpinnerNumberModel(5, 1, null, 1));
+		panneauHaut.add(spinner);
+		panneauHaut.add(boxS);
+		panneauHaut.add(boxM);
+		panneauHaut.add(boxL);
+		panneauHaut.add(boxV);
 		JScrollPane scrollPane = new JScrollPane();
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
 		modStandard = new CardsShakerTableModel();
@@ -126,8 +178,20 @@ public class BestTrendingDashlet extends AbstractJDashlet{
 			try {
 				spinner.setValue(Integer.parseInt(props.getProperty("LIMIT","5")));
 			} catch (Exception e) {
-				//logger.error("can't get LIMIT value",e);
+				logger.error("can't get LIMIT value",e);
 			}
+			
+			try {
+				boxS.setSelected(Boolean.parseBoolean(props.getProperty("STD","false")));
+				boxM.setSelected(Boolean.parseBoolean(props.getProperty("MDN","true")));
+				boxL.setSelected(Boolean.parseBoolean(props.getProperty("LEG","false")));
+				boxV.setSelected(Boolean.parseBoolean(props.getProperty("VIN","false")));
+			} catch (Exception e) {
+				logger.error("can't get LIMIT value",e);
+			}
+			
+			
+			
 			setBounds(r);
 			}
 		
