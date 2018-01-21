11e5047ba9e14b07be60f480e7bfd11d829f331a
nicho92 <nicolas.pihen@gmail.com>
2018-01-21 13:20:22 +0100

    deck draw propability

--- src/main/java/org/magic/tools/DeckCalculator.java
+++ src/main/java/org/magic/tools/DeckCalculator.java
@@ -1,10 +1,12 @@
 package org.magic.tools;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.math3.distribution.HypergeometricDistribution;
 import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicDeck;
 import org.magic.api.exports.impl.MTGDesktopCompanionExport;
@@ -22,30 +24,38 @@ public class DeckCalculator {
 				Xd = the total number of copies of X that you've drawn so far
 		*/
 		
+		/*
+		 * H (n) = C (X, n) * C (Y - X, Z - n) / C (Y, Z)
+			X standing for the number of a certain card that you have in the deck.
+			Y is the number of cards in the deck.
+			Z is the number of cards you are drawing.
+			N is the number you are checking for.
+	 * */
+		
 		int L = deck.getNbCards();
 		int d=7;
 		int XL=4;
 		int Xd=0;
 		int maxTurn=10;
-		double P = 0;
+		HypergeometricDistribution P;	
 		for(MagicCard mc : deck.getMap().keySet())
 		{
-			System.out.println(mc);
+			System.out.println(mc + "(x"+deck.getMap().get(mc)+")");
 			Xd=0;
 			d=7;
 			XL=deck.getMap().get(mc);
 			
-			for(int i=0;i<maxTurn;i++)
+			
+			for(int i=0;i<maxTurn+1;i++)
 			{
 				Xd=Xd+1;
 				if(Xd>=XL)
 					Xd=XL;
 				
+				P= new HypergeometricDistribution(L, XL, d);
+				
+				System.out.print("T"+(i)+"="+new DecimalFormat("#0.0").format(P.upperCumulativeProbability(1)*100)+"%\t");
 				d=d+1;
-				double v1 = XL*d;
-				double v2 = Xd*L;
-				P=v1/v2;
-				System.out.print("T"+(i+1)+"="+NumberFormat.getInstance().format(P*100)+"%\t");
 			}
 			System.out.println("\n");
 		}
