4f1d15d79a74259bc1458d0978e5e3ca875daf19
nicho92 <nicolas.pihen@gmail.com>
2018-01-21 13:47:00 +0100

    deckcalculator formula

--- src/main/java/org/magic/tools/DeckCalculator.java
+++ src/main/java/org/magic/tools/DeckCalculator.java
@@ -13,49 +13,44 @@ import org.magic.api.exports.impl.MTGDesktopCompanionExport;
 
 public class DeckCalculator {
 
+	MagicDeck deck;
+	HypergeometricDistribution hypergeoVar;
+	
+	public DeckCalculator(MagicDeck d) {
+		setDeck(d);
+	}
+	
+	public void setDeck(MagicDeck d)
+	{
+		this.deck=d;
+		
+	}
+	
+	
+	public double getProbability(int turn,MagicCard mc)
+	{
+		int drawedCards=7;
+		
+		if(turn<=0)
+			drawedCards=7;
+		else
+			drawedCards=drawedCards+turn;
+		
+		int numberInDeck=deck.getMap().get(mc);
+		int numberCardsInDeck=deck.getNbCards();
+		hypergeoVar= new HypergeometricDistribution(numberCardsInDeck, numberInDeck, drawedCards);
+		return hypergeoVar.upperCumulativeProbability(1);
+	}
 	
 	public static void main(String[] args) throws Exception {
 		MagicDeck deck = new MTGDesktopCompanionExport().importDeck(new File("C:\\Users\\Nicolas\\.magicDeskCompanion\\decks\\grixis death shadow.deck"));
-		/*
-				P = Probability
-				L = the number of cards in your deck
-				d = the total number of cards you've drawn so far in the game (so, 7 for opening hand, and one more for each extra card drawn so far)
-				XL = the number of copies of card "X" you have in your deck
-				Xd = the total number of copies of X that you've drawn so far
-		*/
+		DeckCalculator calc = new DeckCalculator(deck);
 		
-		/*
-		 * H (n) = C (X, n) * C (Y - X, Z - n) / C (Y, Z)
-			X standing for the number of a certain card that you have in the deck.
-			Y is the number of cards in the deck.
-			Z is the number of cards you are drawing.
-			N is the number you are checking for.
-	 * */
-		
-		int L = deck.getNbCards();
-		int d=7;
-		int XL=4;
-		int Xd=0;
-		int maxTurn=10;
-		HypergeometricDistribution P;	
 		for(MagicCard mc : deck.getMap().keySet())
 		{
-			System.out.println(mc + "(x"+deck.getMap().get(mc)+")");
-			Xd=0;
-			d=7;
-			XL=deck.getMap().get(mc);
-			
-			
-			for(int i=0;i<maxTurn+1;i++)
+			for(int i=0;i<10+1;i++)
 			{
-				Xd=Xd+1;
-				if(Xd>=XL)
-					Xd=XL;
-				
-				P= new HypergeometricDistribution(L, XL, d);
-				
-				System.out.print("T"+(i)+"="+new DecimalFormat("#0.0").format(P.upperCumulativeProbability(1)*100)+"%\t");
-				d=d+1;
+				System.out.print("T"+(i)+"="+new DecimalFormat("#0.0").format(calc.getProbability(i, mc)*100)+"%\t");
 			}
 			System.out.println("\n");
 		}
