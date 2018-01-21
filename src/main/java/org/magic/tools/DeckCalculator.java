7b53f3d0843a2d402bc65406001a81e15274f083
nicho92 <nicolas.pihen@gmail.com>
2018-01-21 13:57:52 +0100

    add function to calculator

--- src/main/java/org/magic/tools/DeckCalculator.java
+++ src/main/java/org/magic/tools/DeckCalculator.java
@@ -1,31 +1,36 @@
 package org.magic.tools;
 
-import java.io.File;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.math3.distribution.HypergeometricDistribution;
 import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicDeck;
-import org.magic.api.exports.impl.MTGDesktopCompanionExport;
 
 public class DeckCalculator {
 
-	MagicDeck deck;
-	HypergeometricDistribution hypergeoVar;
+	private MagicDeck deck;
+	private HypergeometricDistribution hypergeoVar;
+	private DecimalFormat format;
 	
 	public DeckCalculator(MagicDeck d) {
 		setDeck(d);
+		format =new DecimalFormat("#0.0");
 	}
 	
 	public void setDeck(MagicDeck d)
 	{
 		this.deck=d;
-		
 	}
 	
+	public void setFormat(String f)
+	{
+		format = new DecimalFormat(f);
+	}
+	
+	public String format(Double d)
+	{
+		return format.format(d*100)+"%";
+	}
 	
 	public double getProbability(int turn,MagicCard mc)
 	{
@@ -41,21 +46,4 @@ public class DeckCalculator {
 		hypergeoVar= new HypergeometricDistribution(numberCardsInDeck, numberInDeck, drawedCards);
 		return hypergeoVar.upperCumulativeProbability(1);
 	}
-	
-	public static void main(String[] args) throws Exception {
-		MagicDeck deck = new MTGDesktopCompanionExport().importDeck(new File("C:\\Users\\Nicolas\\.magicDeskCompanion\\decks\\grixis death shadow.deck"));
-		DeckCalculator calc = new DeckCalculator(deck);
-		
-		for(MagicCard mc : deck.getMap().keySet())
-		{
-			for(int i=0;i<10+1;i++)
-			{
-				System.out.print("T"+(i)+"="+new DecimalFormat("#0.0").format(calc.getProbability(i, mc)*100)+"%\t");
-			}
-			System.out.println("\n");
-		}
-		
-		
-	}
-	
 }
