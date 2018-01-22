f922a4b8bba0bd0cc484491d4ac279981f1b6bc3
Nicho <nicolas.pihen@gmail.com>
2018-01-22 17:45:39 +0100

    exception for not founded pics

---
+++ src/main/java/org/magic/api/exceptions/PicturesNotFoundException.java
@@ -0,0 +1,16 @@
+package org.magic.api.exceptions;
+
+public class PicturesNotFoundException extends Exception {
+
+	public PicturesNotFoundException() {
+		super();
+	}
+	
+	public PicturesNotFoundException(String message)
+	{
+		super(message);
+	}
+	
+	
+	
+}
