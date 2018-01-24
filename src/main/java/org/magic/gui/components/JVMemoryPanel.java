02cd0c734056aba2c1ea93d7982c85a3196bf381
nicho92 <nicolas.pihen@gmail.com>
2018-01-24 22:12:53 +0100

    jvmpanel enhanced

--- src/main/java/org/magic/gui/components/JVMemoryPanel.java
+++ src/main/java/org/magic/gui/components/JVMemoryPanel.java
@@ -1,7 +1,10 @@
 package org.magic.gui.components;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,7 +17,34 @@ public class JVMemoryPanel extends JPanel
 	private Timer t;
 	private int delay=1000;
 	private String tooltip;
-	private boolean started=true;
+	private TimerTask task ;
+	private Color defaultBack;
+	private Color defaultFront;
+	
+	public void setDelayInMillisecond(int delay)
+	{
+		this.delay=delay;
+		stop();
+		start();
+	}
+	
+	
+	public void stop()
+	{
+		t.cancel();
+	}
+	
+	
+	public void start()
+	{
+		t.scheduleAtFixedRate(task,0,delay);
+	}
+	
+	public void setColor(Color foreground,Color background)
+	{
+		progressBar.setForeground(foreground);
+		progressBar.setBackground(background);
+	}
 	
 	
 	public JVMemoryPanel() {
@@ -25,36 +55,51 @@ public class JVMemoryPanel extends JPanel
 		progressBar.setStringPainted(true);
 		progressBar.setToolTipText(tooltip);
 		add(progressBar);
+		defaultBack=progressBar.getBackground();
+		defaultFront=progressBar.getForeground();
 		
-		{
-
-			t = new Timer();
-			t.scheduleAtFixedRate(new TimerTask() {
-				public void run() {
-					if(started)
-					{ 
-					progressBar.setValue(toMB((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())));
-					tooltip=toMB((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()))+"/"+toMB((Runtime.getRuntime().totalMemory()))+" MB";
-					progressBar.setToolTipText(tooltip);
-					}
-				}
-			},0,delay);
-		}
-		
-	}
-
-	@Override
-	public void paintComponents(Graphics arg0) {
-		super.paintComponents(arg0);
-		
-		if(isVisible())
-			started=true;
-		else
-			started=false;
-		
-		
-	}
+		t = new Timer();
+		task = new TimerTask() {
+			public void run() {
+				progressBar.setValue(toMB((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())));
+				tooltip=toMB((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()))+"/"+toMB((Runtime.getRuntime().totalMemory()))+" MB";
+				progressBar.setToolTipText(tooltip);
+				
+				
+				double pc = (progressBar.getValue()*100)/progressBar.getMaximum();
+				
+				if(pc>55)
+					setColor(Color.ORANGE, Color.ORANGE);
+				else
+				if(pc>75)
+					setColor(Color.BLACK, Color.RED);
+				else
+					setColor(defaultFront, defaultBack);
+					
+				
+			}
+		};
 	
+		start();
+		
+		
+		addComponentListener(new ComponentAdapter() {
+			public void componentShown(ComponentEvent arg0) {
+				System.out.println("start");
+				start();
+			}
+			@Override
+			public void componentHidden(ComponentEvent arg0) {
+				System.out.println("stop");
+				stop();
+			}
+			
+		});
+		
+	
+		
+	}
+
 	private int toMB(double value)
 	{
 		return (int)(value/1024/1024);
