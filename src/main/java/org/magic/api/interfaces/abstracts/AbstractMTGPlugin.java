c843d9b79eb017c07da7ef22e253bc62713e3f0f
nicho92 <nicolas.pihen@gmail.com>
2018-03-04 21:38:05 +0100

    bugfix

--- src/main/java/org/magic/api/interfaces/abstracts/AbstractMTGPlugin.java
+++ src/main/java/org/magic/api/interfaces/abstracts/AbstractMTGPlugin.java
@@ -5,9 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Properties;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.log4j.Logger;
 import org.magic.api.interfaces.MTGPlugin;
 import org.magic.services.MTGLogger;
@@ -37,6 +41,9 @@ public abstract class AbstractMTGPlugin extends Observable implements MTGPlugin{
 			return defaultVal;
 	}
 	
+	public File getConfFile() {
+		return confFile;
+	}
 	
 	public File getConfdir() {
 		return confdir;
