171717c671ee5e89c8d81dc6f31e3781e6631be1
nicho92 <nicolas.pihen@gmail.com>
2018-03-30 19:33:09 +0200

    mtgstock update

--- src/main/java/org/magic/api/dashboard/impl/MTGStockDashBoard.java
+++ src/main/java/org/magic/api/dashboard/impl/MTGStockDashBoard.java
@@ -4,16 +4,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.apache.commons.lang3.StringUtils;
