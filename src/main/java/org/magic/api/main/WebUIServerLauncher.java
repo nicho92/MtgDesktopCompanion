72c282feb3ea8d3c1bb5269cd6e0c3e606916456
nicho92 <nicolas.pihen@gmail.com>
2018-04-03 21:15:51 +0200

    main launcher for webui

---
+++ src/main/java/org/magic/api/main/WebUIServerLauncher.java
@@ -0,0 +1,20 @@
+package org.magic.api.main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.magic.servers.impl.JSONHttpServer;
+import org.magic.servers.impl.WebManagerServer;
+import org.magic.services.MTGControler;
+
+public class WebUIServerLauncher {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+		MTGControler.getInstance().getEnabledProviders().init();
+		MTGControler.getInstance().getEnabledDAO().init();
+		
+		new JSONHttpServer().start();
+		new WebManagerServer().start();
+	}
+
+}
