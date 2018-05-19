444cb762b17e1507e89921e71caba7ff2312d877
nicho92 <nicolas.pihen@gmail.com>
2018-05-20 00:08:39 +0200

    new mkm api

--- src/main/java/org/magic/api/beans/EnumCondition.java
+++ src/main/java/org/magic/api/beans/EnumCondition.java
@@ -2,6 +2,6 @@ package org.magic.api.beans;
 
 public enum EnumCondition {
 
-	MINT, NEAR_MINT, LIGHTLY_PLAYED, PLAYED, POOR;
+	MINT, NEAR_MINT, LIGHTLY_PLAYED, PLAYED, POOR,PROXY;
 
 }
--- src/main/java/org/magic/gui/renderer/AlertedCardsRenderer.java
+++ src/main/java/org/magic/gui/renderer/AlertedCardsRenderer.java
@@ -8,11 +8,9 @@ import javax.swing.table.DefaultTableCellRenderer;
 
 public class AlertedCardsRenderer extends DefaultTableCellRenderer {
 	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,int row, int column) {
 		Component comp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 		comp.setForeground(Color.BLACK);
-
 		if ((Integer) value > 0)
 			comp.setBackground(Color.GREEN);
 		else
