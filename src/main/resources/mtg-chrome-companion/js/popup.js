89f0de03ae3b2a76a3e362e80569b016a14f64fb
Nicho <nicolas.pihen@gmail.com>
2018-06-04 14:04:22 +0200

    popup beta show

--- src/main/resources/mtg-chrome-companion/js/popup.js
+++ src/main/resources/mtg-chrome-companion/js/popup.js
@@ -1,6 +1,15 @@
 
-chrome.storage.sync.get('data', function(data) {
+chrome.storage.sync.get('data', function(d) {
 	
-	console.log(data);
-	document.getElementById('content').html("c");
+	$('#content').append("<h3>"+d.data[0].name+"<h3>");
+	
+	for (var i = 0; i < d.data.length; i++) {
+    	$('#content').append("<b>"+d.data[i].set+"</b>:");
+		for(var y =0; y<d.data[i].collections.length;y++)
+		{
+			$('#content').append(d.data[i].collections[y].name).append(",");
+		}
+		$('#content').append("<br/>");
+   	}
+	$('#cardPic').attr("src", "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid="+d.data[0].multiverse+"&type=card");
 });
