e45dbd19c768ee3e0bfed8bc77f36a5084898d3f
Nicho <nicolas.pihen@gmail.com>
2018-12-06 21:37:11 +0100

    bugfix

--- src/main/java/org/magic/gui/BalanceGUI.java
+++ src/main/java/org/magic/gui/BalanceGUI.java
@@ -2,21 +2,17 @@ package org.magic.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.log4j.Logger;
 import org.jdesktop.swingx.JXTable;
 import org.magic.api.beans.CardPriceVariations;
 import org.magic.api.beans.MTGNotification;
@@ -25,33 +21,22 @@ import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicEdition;
 import org.magic.api.beans.OrderEntry;
 import org.magic.api.beans.OrderEntry.TYPE_TRANSACTION;
-import org.magic.api.exports.impl.JsonExport;
 import org.magic.api.interfaces.MTGCardsProvider;
 import org.magic.api.interfaces.MTGDashBoard;
 import org.magic.gui.abstracts.MTGUIComponent;
+import org.magic.gui.components.OrderEntryPanel;
+import org.magic.gui.components.charts.HistoryPricesPanel;
 import org.magic.gui.components.dialog.OrderImporterDialog;
 import org.magic.gui.models.ShoppingEntryTableModel;
 import org.magic.gui.renderer.MagicEditionJLabelRenderer;
-import org.magic.gui.renderer.MagicEditionsComboBoxCellEditor;
 import org.magic.gui.renderer.OrderEntryRenderer;
-import org.magic.services.FinancialBookService;
 import org.magic.services.MTGConstants;
 import org.magic.services.MTGControler;
-import org.magic.services.MTGLogger;
 import org.magic.services.ThreadManager;
 import org.magic.tools.UITools;
-
-import com.google.gson.reflect.TypeToken;
-import org.magic.gui.components.OrderEntryPanel;
-import org.magic.gui.components.charts.FinancialChartPanel;
-import org.magic.gui.components.charts.HistoryPricesPanel;
-
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.BoxLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Component;
 
 public class BalanceGUI extends MTGUIComponent {
 	
@@ -66,43 +51,11 @@ public class BalanceGUI extends MTGUIComponent {
 	private JXTable table;
 	private OrderEntryPanel orderEntryPanel;
 	private HistoryPricesPanel pricesPanel;
+	private JLabel lblComparator;
+	private JPanel editorPanel;
+	private JPanel panelComparator;
 
-	private void calulate(List<OrderEntry> entries)
-	{
-		double totalS=0;
-		double totalB=0;
-
-		for(OrderEntry e : entries)
-		{
-			if(e.getTypeTransaction().equals(TYPE_TRANSACTION.BUY))
-				totalB=totalB+e.getItemPrice();
-			else
-				totalS=totalS+e.getItemPrice();
-		}
 	
-		if(entries.size()<model.getRowCount())
-		{
-			selectionBuy.setText(UITools.formatDouble(totalB));
-			selectionSell.setText(UITools.formatDouble(totalS));
-			totalSelection.setText(": "+UITools.formatDouble(totalS-totalB)+")");
-			if((totalS-totalB)>0)
-				totalSelection.setIcon(MTGConstants.ICON_UP);
-			else
-				totalSelection.setIcon(MTGConstants.ICON_DOWN);
-			
-		}
-		else
-		{
-			totalBuy.setText(UITools.formatDouble(totalB));
-			totalSell.setText(UITools.formatDouble(totalS));
-			total.setText(UITools.formatDouble(totalS-totalB));
-			
-			if((totalS-totalB)>0)
-				total.setIcon(MTGConstants.ICON_UP);
-			else
-				total.setIcon(MTGConstants.ICON_DOWN);
-		}
-	}
 	
 	private void loadFinancialBook()
 	{
@@ -116,8 +69,6 @@ public class BalanceGUI extends MTGUIComponent {
 	
 	public BalanceGUI() {
 		
-		
-		
 		JPanel panneauBas = new JPanel();
 		JPanel panneauHaut = new JPanel();
 		JPanel panneauRight = new JPanel();
@@ -125,11 +76,6 @@ public class BalanceGUI extends MTGUIComponent {
 		model = new ShoppingEntryTableModel();
 		JButton btnImportTransaction = new JButton(MTGConstants.ICON_IMPORT);
 		JButton btnSave = new JButton(MTGConstants.ICON_SAVE);
-		orderEntryPanel = new OrderEntryPanel();
-		JButton btnSaveOrder = new JButton(MTGConstants.ICON_SAVE);
-		JPanel panelButton = new JPanel();
-		JButton btnDeleteOrder = new JButton(MTGConstants.ICON_DELETE);
-		JButton btnNewEntry = new JButton(MTGConstants.ICON_NEW);
 		totalBuy = new JLabel(MTGConstants.ICON_DOWN);
 		totalSell = new JLabel(MTGConstants.ICON_UP);
 		total = new JLabel();
@@ -137,9 +83,6 @@ public class BalanceGUI extends MTGUIComponent {
 		selectionSell = new JLabel(MTGConstants.ICON_UP);
 		selectionBuy=new JLabel(MTGConstants.ICON_DOWN);
 		pricesPanel = new HistoryPricesPanel(false);
-		
-	
-		btnDeleteOrder.setEnabled(false);
 		btnSave.setEnabled(false);
 		UITools.initTableFilter(table);
 		model.setWritable(true);
@@ -164,13 +107,26 @@ public class BalanceGUI extends MTGUIComponent {
 		add(panneauHaut, BorderLayout.NORTH);
 		add(new JScrollPane(table), BorderLayout.CENTER);
 		add(panneauRight,BorderLayout.EAST);
+		
 		panneauRight.setLayout(new BoxLayout(panneauRight, BoxLayout.Y_AXIS));
-		panneauRight.add(orderEntryPanel);
-		panneauRight.add(panelButton);
-		panneauRight.add(pricesPanel);
+		
+		editorPanel = new JPanel();
+		editorPanel.setAlignmentY(Component.TOP_ALIGNMENT);
+		panneauRight.add(editorPanel);
+		editorPanel.setLayout(new BorderLayout(0, 0));
+		orderEntryPanel = new OrderEntryPanel();
+		editorPanel.add(orderEntryPanel, BorderLayout.CENTER);
+		JButton btnSaveOrder = new JButton(MTGConstants.ICON_SAVE);
+		JPanel panelButton = new JPanel();
+		editorPanel.add(panelButton, BorderLayout.SOUTH);
+		JButton btnDeleteOrder = new JButton(MTGConstants.ICON_DELETE);
+		JButton btnNewEntry = new JButton(MTGConstants.ICON_NEW);
+		
+	
+		btnDeleteOrder.setEnabled(false);
+		
 		panelButton.add(btnSaveOrder);
 		panelButton.add(btnNewEntry);
-		add(panneauBas,BorderLayout.SOUTH);
 		panelButton.add(btnDeleteOrder);
 		
 		btnSaveOrder.addActionListener(ae->{
@@ -188,6 +144,17 @@ public class BalanceGUI extends MTGUIComponent {
 			model.removeItem(UITools.getTableSelection(table, 0));
 			calulate(model.getItems());
 		});
+		panneauRight.add(pricesPanel);
+		
+		panelComparator = new JPanel();
+		panneauRight.add(panelComparator);
+		panelComparator.setLayout(new BorderLayout(0, 0));
+		
+		lblComparator = new JLabel("Values");
+		panelComparator.add(lblComparator);
+		lblComparator.setHorizontalAlignment(SwingConstants.LEFT);
+		lblComparator.setFont(MTGConstants.FONT.deriveFont(Font.BOLD, 16));
+		add(panneauBas,BorderLayout.SOUTH);
 		
 		
 		loadFinancialBook();
@@ -208,7 +175,27 @@ public class BalanceGUI extends MTGUIComponent {
 						catch(Exception e)
 						{
 							//do nothing
+						}	
+						Currency source = MTGControler.getInstance().getEnabled(MTGDashBoard.class).getCurrency();
+						CardPriceVariations e;
+						try {
+							e = MTGControler.getInstance().getEnabled(MTGDashBoard.class).getPriceVariation(mc, o.getEdition());
+						
+						Double actualValue = MTGControler.getInstance().getCurrencyService().convert(source, o.getCurrency(), e.get(e.getLastDay()));
+						
+						lblComparator.setText(o.getCurrency() + " VALUE="+UITools.formatDouble(actualValue) + " PAID=" + UITools.formatDouble(o.getItemPrice()));
+						
+						if(actualValue<o.getItemPrice())
+							lblComparator.setIcon(MTGConstants.ICON_DOWN);
+						else if(actualValue>o.getItemPrice())
+							lblComparator.setIcon(MTGConstants.ICON_UP);
+						else
+							lblComparator.setIcon(null);
+						} catch (IOException e1) {
+							//do nothing
 						}
+							
+						
 						
 						pricesPanel.init(mc, o.getEdition(), o.getDescription());
 						pricesPanel.revalidate();
@@ -263,6 +250,42 @@ public class BalanceGUI extends MTGUIComponent {
 		return MTGConstants.ICON_SHOP;
 	}
 	
+	private void calulate(List<OrderEntry> entries)
+	{
+		double totalS=0;
+		double totalB=0;
+
+		for(OrderEntry e : entries)
+		{
+			if(e.getTypeTransaction().equals(TYPE_TRANSACTION.BUY))
+				totalB=totalB+e.getItemPrice();
+			else
+				totalS=totalS+e.getItemPrice();
+		}
+	
+		if(entries.size()<model.getRowCount())
+		{
+			selectionBuy.setText(UITools.formatDouble(totalB));
+			selectionSell.setText(UITools.formatDouble(totalS));
+			totalSelection.setText(": "+UITools.formatDouble(totalS-totalB)+")");
+			if((totalS-totalB)>0)
+				totalSelection.setIcon(MTGConstants.ICON_UP);
+			else
+				totalSelection.setIcon(MTGConstants.ICON_DOWN);
+			
+		}
+		else
+		{
+			totalBuy.setText(UITools.formatDouble(totalB));
+			totalSell.setText(UITools.formatDouble(totalS));
+			total.setText(UITools.formatDouble(totalS-totalB));
+			
+			if((totalS-totalB)>0)
+				total.setIcon(MTGConstants.ICON_UP);
+			else
+				total.setIcon(MTGConstants.ICON_DOWN);
+		}
+	}
 	
 
 }
