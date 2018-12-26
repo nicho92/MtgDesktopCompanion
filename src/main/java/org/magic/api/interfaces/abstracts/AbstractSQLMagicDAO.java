084bd1d3f24cbb18019425d8d127a2e14951e3b3
nicho92 <nicolas.pihen@gmail.com>
2018-12-26 22:11:30 +0100

    abstract dao sql

--- src/main/java/org/magic/api/interfaces/abstracts/AbstractSQLMagicDAO.java
+++ src/main/java/org/magic/api/interfaces/abstracts/AbstractSQLMagicDAO.java
@@ -56,12 +56,14 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 
 	
 	public abstract String getAutoIncrementKeyWord();
+	public abstract String getjdbcnamedb();
 	 
-	 
+	
+	
 	 
 	public void init() throws SQLException, ClassNotFoundException {
 		logger.info("init " + getName());
-		String url = "jdbc:mysql://" + getString(SERVERNAME) + ":" + getString(SERVERPORT);
+		String url = "jdbc:"+getjdbcnamedb()+"://" + getString(SERVERNAME) + ":" + getString(SERVERPORT);
 		logger.trace("Connexion to " + url + "/" + getString(DB_NAME) + getString(PARAMS));
 		con = DriverManager.getConnection(url + "/" + getString(DB_NAME) + getString(PARAMS),getString(LOGIN), getString(PASS));
 		createDB();
@@ -208,14 +210,11 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 
 	@Override
 	public List<MagicCard> listCardsFromCollection(MagicCollection collection, MagicEdition me) throws SQLException {
-		Chrono c = new Chrono();
-		
 		String sql = "select "+MCARD+" from cards where collection= ?";
 
 		if (me != null)
 			sql = "select "+MCARD+" from cards where collection= ? and edition = ?";
 
-		c.start();
 		logger.trace(sql +" " + collection +" " + me);
 
 		try (PreparedStatement pst = con.prepareStatement(sql)) {
@@ -230,7 +229,7 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 					MagicCard mc = serialiser.fromJson(rs.getString(MCARD),MagicCard.class);
 					ret.add(mc);
 				}
-				logger.trace(sql +" query done in " + c.stop() +"s");
+				
 				return ret;
 			}
 		}
@@ -317,29 +316,6 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 			pst.setString(2, col.getName());
 			pst.executeUpdate();
 		}
-	}
-
-	@Override
-	public String getDBLocation() {
-		return getString(SERVERNAME) + "/" + getString(DB_NAME);
-	}
-
-	@Override
-	public long getDBSize() {
-		String sql = "SELECT Round(Sum(data_length + index_length), 1) FROM   information_schema.tables WHERE  table_schema = 'mtgdesktopclient'";
-		try (PreparedStatement pst = con.prepareStatement(sql); ResultSet rs = pst.executeQuery();) {
-			rs.first();
-			return (long) rs.getDouble(1);
-		} catch (SQLException e) {
-			logger.error(e);
-			return 0;
-		}
-
-	}
-
-	@Override
-	public String getName() {
-		return "MySQL";
 	}
 
 	@Override
@@ -513,37 +489,6 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 		}
 	}
 
-	@Override
-	public void backup(File f) throws SQLException, IOException {
-
-		if (getString(MYSQL_DUMP_PATH).length() <= 0)
-			throw new NullPointerException("Please fill MYSQL_DUMP_PATH var");
-
-		if (!new File(getString(MYSQL_DUMP_PATH)).exists())
-			throw new IOException(getString(MYSQL_DUMP_PATH) + " doesn't exist");
-
-		StringBuilder dumpCommand = new StringBuilder();
-		dumpCommand.append(getString(MYSQL_DUMP_PATH)).append("/mysqldump ").append(getString(DB_NAME))
-				   .append(" -h ").append(getString(SERVERNAME))
-				   .append(" -u ").append(getString(LOGIN))
-				   .append(" -p").append(getString(PASS))
-				   .append(" --port ").append(getString(SERVERPORT));
-		
-		Runtime rt = Runtime.getRuntime();
-		logger.info("begin Backup " + getString(DB_NAME));
-		Process child;
-
-		child = rt.exec(dumpCommand.toString());
-		try (PrintStream ps = new PrintStream(f)) {
-			InputStream in = child.getInputStream();
-			int ch;
-			while ((ch = in.read()) != -1) {
-				ps.write(ch);
-			}
-			logger.info("Backup " + getString(DB_NAME) + " done");
-		}
-
-	}
 
 	@Override
 	public void initAlerts() {
@@ -675,31 +620,7 @@ public abstract class AbstractSQLMagicDAO extends AbstractMagicDAO {
 
 	}
 
-	@Override
-	public void initDefault() {
-		setProperty(DRIVER, "com.mysql.jdbc.Driver");
-		setProperty(SERVERNAME, "localhost");
-		setProperty(SERVERPORT, "3306");
-		setProperty(DB_NAME, "mtgdesktopclient");
-		setProperty(LOGIN, "login");
-		setProperty(PASS, "");
-		setProperty(PARAMS, "?autoDeserialize=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true");
-		setProperty(MYSQL_DUMP_PATH, "C:\\Program Files (x86)\\Mysql\\bin");
-
-	}
-
-	@Override
-	public String getVersion() {
-		
-		try {
-			Driver d = new com.mysql.cj.jdbc.Driver();
-			return d.getMajorVersion()+"."+d.getMinorVersion();
-		} catch (SQLException e) {
-			return "1.0";
-		}
-		
-	}
-
+	
 	@Override
 	public void initOrders() {
 		try (PreparedStatement pst = con.prepareStatement("select * from orders"); ResultSet rs = pst.executeQuery();) {
