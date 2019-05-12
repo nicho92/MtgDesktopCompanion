c5025f054baf3a0a62f9f07523c1f8f60006615f
nicho92 <nicolas.pihen@gmail.com>
2019-05-12 16:44:34 +0200

    source clean

--- src/main/java/org/magic/gui/renderer/NewsTreeCellRenderer.java
+++ src/main/java/org/magic/gui/renderer/NewsTreeCellRenderer.java
@@ -2,34 +2,33 @@ package org.magic.gui.renderer;
 
 import java.awt.Component;
 
+import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeCellRenderer;
 
 import org.magic.api.beans.MagicNews;
 import org.magic.services.MTGConstants;
 
-public class NewsTreeCellRenderer extends DefaultTreeCellRenderer {
-
-	private static final long serialVersionUID = 1L;
-
+public class NewsTreeCellRenderer implements TreeCellRenderer {
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,boolean isLeaf, int row, boolean focused) {
-		Component c = super.getTreeCellRendererComponent(tree, value, selected, expanded, isLeaf, row, focused);
+		JLabel c = (JLabel)new DefaultTreeCellRenderer().getTreeCellRendererComponent(tree, value, selected, expanded, isLeaf, row, focused);
 
 		if (((DefaultMutableTreeNode) value).getUserObject() instanceof MagicNews) {
 			try {
-				setIcon(((MagicNews) ((DefaultMutableTreeNode) value).getUserObject()).getProvider().getIcon());
+				c.setIcon(((MagicNews) ((DefaultMutableTreeNode) value).getUserObject()).getProvider().getIcon());
 			}
 			catch(Exception e)
 			{
-				setIcon(null);
+				c.setIcon(null);
 			}
 
 		}
 
 		if (((DefaultMutableTreeNode) value).getUserObject() instanceof String)
-			setIcon(MTGConstants.ICON_NEWS);
+			c.setIcon(MTGConstants.ICON_NEWS);
 
 		return c;
 	}
