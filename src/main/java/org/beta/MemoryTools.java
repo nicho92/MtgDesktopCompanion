756a41ccffb5bf55a256617c499d009f77e5d01e
nicho92 <nicolas.pihen@gmail.com>
2019-05-13 23:31:03 +0200

    memory tools

--- src/main/java/org/beta/MemoryTools.java
+++ src/main/java/org/beta/MemoryTools.java
@@ -10,8 +10,11 @@ import org.magic.services.MTGControler;
 import org.openjdk.jol.info.GraphLayout;
 import org.openjdk.jol.util.Multiset;
 
-public class MemoryTest {
+public class MemoryTools {
 
+	
+	static long total=0;
+	
 	
 	
 	
@@ -20,15 +23,19 @@ public class MemoryTest {
 		
 		prov.init();
 		List<MagicCard> mcs = prov.searchCardByEdition(new MagicEdition("WAR"));
-		Multiset<Class<?>> size = GraphLayout.parseInstance(mcs.get(0)).getClassSizes();
-		Multiset<Class<?>> count =GraphLayout.parseInstance(mcs.get(0)).getClassCounts(); 
+		Multiset<Class<?>> size = GraphLayout.parseInstance(mcs).getClassSizes();
+		Multiset<Class<?>> count =GraphLayout.parseInstance(mcs).getClassCounts(); 
+		
+		total=0;
 		
 		size.keys().forEach(cl->{
 			
 			long s=size.count(cl);
 			long c=count.count(cl);
-			
-			System.out.println("SUM=" + s + "\tCOUNT=" + c + "\t"+cl);
+			long avg=s/c;
+			System.out.println("COUNT=" + c + "\tAVG=" + avg + "\tSUM="+s+ "\t"+cl.getName());
+			total+=s;
 		});
+		System.out.println("TOTAL="+(total/1024/1024)+"MB");
     }
 }
