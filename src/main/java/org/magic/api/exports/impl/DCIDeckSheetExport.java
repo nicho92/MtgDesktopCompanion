de573efd2a10150f48163cb54031545f193da566
nicho92 <nicolas.pihen@gmail.com>
2019-09-01 18:17:24 +0200

    bugfix

--- src/main/java/org/magic/api/exports/impl/DCIDeckSheetExport.java
+++ src/main/java/org/magic/api/exports/impl/DCIDeckSheetExport.java
@@ -3,7 +3,6 @@ package org.magic.api.exports.impl;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
