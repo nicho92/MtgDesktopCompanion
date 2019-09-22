e15de2e4138876d0e9fdfeeeeb00341b13ee9576
nicho92 <nicolas.pihen@gmail.com>
2019-09-22 21:29:32 +0200

    log pandomium

--- src/main/java/org/magic/gui/components/browser/ChromiumBrowserComponent.java
+++ src/main/java/org/magic/gui/components/browser/ChromiumBrowserComponent.java
@@ -8,6 +8,7 @@ import org.magic.gui.abstracts.MTGUIBrowserComponent;
 import org.magic.services.MTGConstants;
 import org.panda_lang.pandomium.Pandomium;
 import org.panda_lang.pandomium.settings.PandomiumSettings;
+import org.panda_lang.pandomium.util.os.PandomiumOS;
 import org.panda_lang.pandomium.wrapper.PandomiumBrowser;
 import org.panda_lang.pandomium.wrapper.PandomiumClient;
 
@@ -30,7 +31,7 @@ public class ChromiumBrowserComponent extends MTGUIBrowserComponent {
 		try {
 			
 			Pandomium pandomium = new Pandomium(setts);
-			logger.debug("loading pandomium with " + pandomium.getLoader());
+			logger.debug("loading pandomium for " + PandomiumOS.getOS());
 			
 			pandomium.initialize();
 			
