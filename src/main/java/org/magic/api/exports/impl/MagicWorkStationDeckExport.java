96e9ef2ed41881f3b16812ecb80a69b4923d5fe9
Nicho <nicolas.pihen@gmail.com>
2019-12-11 09:52:10 +0100

    MagicWorkstation Update

--- src/main/java/org/magic/api/exports/impl/MagicWorkStationDeckExport.java
+++ src/main/java/org/magic/api/exports/impl/MagicWorkStationDeckExport.java
@@ -8,10 +8,17 @@ import java.io.StringReader;
 
 import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicDeck;
+import org.magic.api.beans.MagicEdition;
+import org.magic.api.interfaces.MTGCardsProvider;
 import org.magic.api.interfaces.abstracts.AbstractCardExport;
+import org.magic.api.interfaces.abstracts.AbstractFormattedFileCardExport;
+import org.magic.services.MTGControler;
 
-public class MagicWorkStationDeckExport extends AbstractCardExport {
+public class MagicWorkStationDeckExport extends AbstractFormattedFileCardExport {
 
+	
+	
+	
 	@Override
 	public String getFileExtension() {
 		return ".mwDeck";
@@ -46,31 +53,49 @@ public class MagicWorkStationDeckExport extends AbstractCardExport {
 		}
 
 	}
-
+	
 	@Override
 	public MagicDeck importDeck(String f,String name) throws IOException {
 		try (BufferedReader read = new BufferedReader(new StringReader(f))) {
 			MagicDeck deck = new MagicDeck();
 			deck.setName(name);
-
-			String line = read.readLine();
-
-			while (line != null) {
-				line = line.trim();
-				if (!line.startsWith("//")) {
-					int qte = 0;
-					MagicCard mc = null;
-					notify(mc);
-					// TODO line parse
-
-					if (line.startsWith("SB"))
-						deck.getMapSideBoard().put(mc, qte);
-					else
-						deck.getMap().put(mc, qte);
-
+			matches(f).forEach(m->{
+				
+				MagicEdition ed = null;
+				try {			   
+					ed = MTGControler.getInstance().getEnabled(MTGCardsProvider.class).getSetById(m.group(3));
 				}
-				line = read.readLine();
-			}
+				catch(Exception e)
+				{
+					logger.error("Edition not found for " + m.group(3));
+				}
+				
+				String cname=cleanName(m.group(4));
+				
+				try {
+					MagicCard mc = MTGControler.getInstance().getEnabled(MTGCardsProvider.class).searchCardByName(cname, ed, true).get(0);
+					int qte = Integer.parseInt(m.group("2"));
+					
+					if(m.group(1).trim().startsWith("SB"))
+					{
+						deck.getMapSideBoard().put(mc, qte);
+					}
+					else
+					{
+						deck.getMap().put(mc, qte);
+					}
+					
+					notify(mc);
+					
+				} catch (Exception e) {
+					logger.error("no card found for " + cname);
+				}
+				
+				System.out.println(m.group());
+				
+				
+			});
+			
 			return deck;
 		}
 	}
@@ -84,13 +109,33 @@ public class MagicWorkStationDeckExport extends AbstractCardExport {
 
 	@Override
 	public STATUT getStatut() {
-		return STATUT.DEV;
+		return STATUT.DEPRECATED;
 	}
 
 	@Override
 	public void initDefault() {
 		// nothing to do
 
+	}
+
+	@Override
+	protected boolean skipFirstLine() {
+		return false;
+	}
+
+	@Override
+	protected String[] skipLinesStartWith() {
+		return new String[] {"//"};
+	}
+
+	@Override
+	protected String getStringPattern() {
+		return "(SB: |\\s)?(\\d+)(?: )?\\[(.*?)\\](.*)";
+	}
+
+	@Override
+	protected String getSeparator() {
+		return null;
 	}
 
 
