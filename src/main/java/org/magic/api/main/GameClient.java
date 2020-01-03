29b1a0d43e3f7e636aba821523d1bd9da0098212
Nicho <nicolas.pihen@gmail.com>
2020-01-03 09:44:15 +0100

    source clean

--- src/main/java/org/magic/api/main/GameClient.java
+++ src/main/java/org/magic/api/main/GameClient.java
@@ -1,31 +1,16 @@
 package org.magic.api.main;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-
 import org.magic.api.interfaces.MTGCardsProvider;
 import org.magic.gui.GameGUI;
-import org.magic.services.MTGConstants;
+import org.magic.gui.abstracts.MTGUIComponent;
 import org.magic.services.MTGControler;
 import org.magic.services.threads.ThreadManager;
 
 public class GameClient {
 
 	public static void main(String[] args) {
-		
 		MTGControler.getInstance().getEnabled(MTGCardsProvider.class).init();
-		
-		ThreadManager.getInstance().invokeLater(() -> {
-			JFrame f = new JFrame(MTGControler.getInstance().getLangService().getCapitalize("GAME_MODULE"));
-			f.setIconImage(MTGConstants.ICON_GAME.getImage());
-			f.getContentPane().add(new GameGUI());
-			f.setVisible(true);
-			f.pack();
-			f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		});
-		
-		
-		
+		ThreadManager.getInstance().invokeLater(() -> MTGUIComponent.createJFrame(new GameGUI(), true, false,true).setVisible(true));
 	}
 	
 }
