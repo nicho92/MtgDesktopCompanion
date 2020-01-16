d6234dce68171ad646af1b7eac3d430c4e2c287e
Nicho <nicolas.pihen@gmail.com>
2020-01-16 19:43:59 +0100

    gedEntryComponent

--- src/main/java/org/magic/gui/components/renderer/GedEntryComponent.java
+++ src/main/java/org/magic/gui/components/renderer/GedEntryComponent.java
@@ -1,6 +1,9 @@
 package org.magic.gui.components.renderer;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.SystemColor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
@@ -18,7 +21,7 @@ public class GedEntryComponent<T> extends JLabel {
 	private static final long serialVersionUID = 1L;
 	private GedEntry<T> entry;
 	private boolean selected = false;
-	
+	private Color defaultColor;
 	public boolean isSelected() {
 		return selected;
 	}
@@ -29,16 +32,26 @@ public class GedEntryComponent<T> extends JLabel {
 		setText(entry.getName());
 		setIcon(entry.getIcon());
 		
+		defaultColor = getBackground();
+		
 		if(entry.isImage())
 			setIcon(getThumbnail());
 		
 		setVerticalTextPosition(SwingConstants.BOTTOM);
 		setHorizontalTextPosition(SwingConstants.CENTER);
 		
+		setToolTipText(e.getName());
+		setOpaque(true);
+		setPreferredSize(new Dimension(75,75));
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent me) {
 				selected=!selected;
+				
+				if(selected)
+					setBackground(SystemColor.activeCaption);
+				else
+					setBackground(defaultColor);
 			}
 		});
 	}
