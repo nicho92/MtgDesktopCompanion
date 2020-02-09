69b56e0d3d444ba26cacf91cb2cf3088e370efed
nicho92 <nicolas.pihen@gmail.com>
2020-02-09 10:14:47 +0100

    deckedbuilder alpha

---
+++ src/main/java/org/beta/DeckedBuilder.java
@@ -0,0 +1,77 @@
+package org.beta;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.magic.api.beans.MagicCard;
+import org.magic.api.beans.MagicDeck;
+import org.magic.api.interfaces.abstracts.AbstractCardExport;
+import org.magic.api.interfaces.abstracts.AbstractFormattedFileCardExport;
+
+public class DeckedBuilder extends AbstractFormattedFileCardExport {
+
+	
+	private final String columns ="Total Qty,Reg Qty,Foil Qty,Card,Set,Mana Cost,Card Type,Color,Rarity,Mvid,Single Price,Single Foil Price,Total Price,Price Source,Notes";
+	
+	@Override
+	public String getFileExtension() {
+		return ".csv";
+	}
+
+	@Override
+	public String getName() {
+		return "DeckedBuilder";
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new DeckedBuilder().importDeckFromFile(new File("D:\\Téléchargements\\ELD.csv"));
+	}
+
+	@Override
+	public void exportDeck(MagicDeck deck, File dest) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MagicDeck importDeck(String f, String dname) throws IOException {
+		MagicDeck deck = new MagicDeck();
+		deck.setName(dname);
+		
+		matches(f,true).forEach(m->{
+			
+			
+			System.out.println(m.group());
+			
+			Integer qty = Integer.parseInt(m.group(1));
+//			MagicCard mc = parseMatcherWithGroup(m, 2, 3, true, false);
+//			if(mc!=null)
+//			{
+//				deck.getMap().put(mc, qty);
+//				notify(mc);
+//			}
+		});
+		return deck;
+	}
+
+	@Override
+	protected boolean skipFirstLine() {
+		return true;
+	}
+
+	@Override
+	protected String[] skipLinesStartWith() {
+		return new String[0];
+	}
+
+	@Override
+	protected String getStringPattern() {
+		return "(\\d+),(\\d+),(\\d+),((?=\\\")\\\".*?\\\"|.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(\\d+),(\\d+.\\d+?),(\\d+.\\d+?),(\\d+.\\d+?)?,(.*?),";
+	}
+
+	@Override
+	protected String getSeparator() {
+		return ",";
+	}
+
+}
