009f7563f33340e4764f637ca5ab1ae5e691e34b
nicho92 <nicolas.pihen@gmail.com>
2020-07-14 19:57:21 +0200

    begining criteria

--- src/main/java/org/magic/api/criterias/CriteriaBuilder.java
+++ src/main/java/org/magic/api/criterias/CriteriaBuilder.java
@@ -1,42 +1,77 @@
 package org.magic.api.criterias;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
-import org.magic.api.beans.enums.MTGColor;
 import org.magic.api.criterias.Criteria.OPERATOR;
 
 public class CriteriaBuilder {
-
-	List<Criteria<?>> builder;
+	public enum FORMAT { SQL, JSON};
+	
+	private FORMAT f;
+	private Criteria<?>[] crits;
 	
 	
-	public CriteriaBuilder(Criteria<?> c)
+	public CriteriaBuilder(FORMAT f, Criteria<?>[] crits)
 	{
-		builder = new ArrayList<>();
-		
-		builder.add(c);
+		this.crits = crits;
+		this.f = f;
+	}
+
+	public String build()
+	{
+		if(f.equals(FORMAT.SQL))
+			return buildSQL();
+		else
+			return buildJSON();
 	}
 	
-	public void and(Criteria<?> c)
-	{
-		builder.add(c);
+	private String buildJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String buildSQL() {
+		
+		StringBuilder temp = new StringBuilder();
+		
+		for(Criteria<?> c : crits)
+		{
+			temp.append(c.getAtt()).append(" ");
+			temp.append(sql(c.getOperator())).append(" ");
+			
+			if(!c.isList())
+			{
+				temp.append(c.getFirst());
+			}
+			else
+			{
+				temp.append(StringUtils.join(c.getVal(), ","));
+			}
+			
+			temp.append(" AND ");
+		}
+		
+		return temp.toString();
+		
+	}
+
+	private String sql(OPERATOR operator) {
+		
+		switch(operator) {
+			case EQ : return "=";
+			case GREATER : return ">";
+			case HAS : return "in";
+			case LIKE : return "like";
+			case LOWER : return "<";
+		}
+		
+		return "=";
+		
+		
 	}
 
 	@Override
 	public String toString() {
-		return StringUtils.join(builder.toArray(), " AND ");
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		CriteriaBuilder build = new CriteriaBuilder(new Criteria<>("name",OPERATOR.LIKE,"Liliana"));
-		build.and(new Criteria<>("color", OPERATOR.HAS, MTGColor.BLACK,MTGColor.GOLD));
-		build.and(new Criteria<>("cmc", OPERATOR.GREATER, 3 ));
-		
-		System.out.println(build.toString());
+		return StringUtils.join(crits, " AND ");
 	}
 	
 }
