4f9aabecf664aa4c578c4de7da9edfc70975475b
nicho92 <nicolas.pihen@gmail.com>
2020-07-15 22:41:22 +0200

    sqlcrit sample

--- src/main/java/org/magic/api/criterias/SQLCriteriaBuilder.java
+++ src/main/java/org/magic/api/criterias/SQLCriteriaBuilder.java
@@ -5,9 +5,10 @@ import static org.jooq.impl.DSL.field;
 import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.table;
 
-import java.util.Arrays;
-
 import org.jooq.Query;
+import org.jooq.Record3;
+import org.jooq.SelectWhereStep;
+import org.magic.api.beans.enums.MTGColor;
 import org.magic.api.criterias.Criteria.OPERATOR;
 
 
@@ -20,17 +21,22 @@ public class SQLCriteriaBuilder
 	public String build(Criteria<?>... crits)
 	{
 		query = select(field("*"))
-                	  .from(table("CARDS"));
-                
+                .from(table("cards"));
 		
+			for(Criteria c:crits)
+					((SelectWhereStep<Record3<Object, Object, Object>>) query).where(field(c.getAtt()).eq(c.getFirst()));
 		
-		
-		
-	
 		return query.toString();
 	}
 	
 
+	public static void main(String[] args) {
+		Criteria<Integer> cmcC = new Criteria<>("cmc",OPERATOR.GREATER,3);
+		Criteria<MTGColor> cmcA = new Criteria<>("colors",OPERATOR.HAS,MTGColor.BLACK,MTGColor.RED);
+		
+		System.out.println(new SQLCriteriaBuilder().build(cmcC,cmcA));
+	}
+	
 	
 	
 }
