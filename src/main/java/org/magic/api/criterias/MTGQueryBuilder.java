ac6c970ebc3d7d126e07ac94efc049aacbce997d
Nicho <nicolas.pihen@gmail.com>
2020-07-17 18:59:48 +0200

    trying to search in array

--- src/main/java/org/magic/api/criterias/MTGQueryBuilder.java
+++ src/main/java/org/magic/api/criterias/MTGQueryBuilder.java
@@ -18,6 +18,8 @@ public interface MTGQueryBuilder<T> {
 		MTGCrit<Integer> cmcI = new MTGCrit<>("convertedManaCost",OPERATOR.GREATER_EQ,6);
 		MTGCrit<String> cmcC = new MTGCrit<>("colors",OPERATOR.IN,MTGColor.BLACK.getCode());
 
+		
+		//$.data..cards[?(@['colors'].indexOf('R') != -1)]
 		Mtgjson5Provider prov = new Mtgjson5Provider();
 		prov.init();
 		prov.searchByCriteria(cmcC).forEach(c->{
