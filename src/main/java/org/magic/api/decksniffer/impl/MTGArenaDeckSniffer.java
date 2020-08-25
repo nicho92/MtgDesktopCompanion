2952e9cd1cd88f837544827790d55a9d799ff69c
nicho92 <nicolas.pihen@gmail.com>
2020-08-25 22:31:34 +0200

    arena update

--- src/main/java/org/magic/api/decksniffer/impl/MTGArenaDeckSniffer.java
+++ src/main/java/org/magic/api/decksniffer/impl/MTGArenaDeckSniffer.java
@@ -5,12 +5,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.magic.api.beans.MagicCard;
 import org.magic.api.beans.MagicDeck;
 import org.magic.api.beans.RetrievableDeck;
+import org.magic.api.interfaces.MTGCardsProvider;
 import org.magic.api.interfaces.abstracts.AbstractDeckSniffer;
+import org.magic.services.MTGControler;
 import org.magic.tools.MTGArenaTools;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class MTGArenaDeckSniffer extends AbstractDeckSniffer {
@@ -47,19 +52,52 @@ public class MTGArenaDeckSniffer extends AbstractDeckSniffer {
 				  d.setDescription(info.getDescription());
 				  
 		
-				  
+			arena.readDecks().get("payload").getAsJsonArray().forEach(je->{
+				JsonObject obj = je.getAsJsonObject();
+				if(obj.get("id").getAsString().equals(info.getUrl().toString()))
+				{
+					load(obj,d.getMain(),"mainDeck");
+					load(obj,d.getSideBoard(),"sideboard");
+					
+					
+				}
+			});
 				  
 				  
 		
 		return d;
 	}
 	
-	public static void main(String[] args) throws IOException {
+	private void load(JsonObject obj, Map<MagicCard, Integer> map, String string) {
+		JsonArray arr = obj.get(string).getAsJsonArray();
 		
-		MTGArenaDeckSniffer sniff = new MTGArenaDeckSniffer();
+		List<String> ids= new ArrayList<>();
+		List<Integer> qtys = new ArrayList<>();
 		
-		RetrievableDeck d = sniff.getDeckList().get(0);
-		sniff.getDeck(d);
+		
+		for(int i=0;i<arr.size();i=i+2)
+		{
+			ids.add(arr.get(i).getAsString());
+		}
+		
+		for(int i=1;i<arr.size();i=i+2)
+		{
+			qtys.add(arr.get(i).getAsInt());
+		}
+		
+		logger.debug("found " + ids.size() +" ids and " + qtys.size() +" qty");
+		
+		
+		for(int i=0;i<ids.size();i++)
+		{
+			try {
+				MagicCard mc = MTGControler.getInstance().getEnabled(MTGCardsProvider.class).getCardByArenaId(ids.get(i));
+				map.put(mc, qtys.get(i));
+				notify(mc);
+			} catch (IOException e) {
+				logger.error("no cards found for " + ids.get(i));
+			}
+		}
 		
 	}
 
