f1a2d88fd228e5cd38cce8d28ab7b5fb08878ddf
nicho92 <nicolas.pihen@gmail.com>
2020-10-21 23:37:42 +0200

    source clean

--- src/main/java/org/magic/servers/impl/PricesCheckerTimer.java
+++ src/main/java/org/magic/servers/impl/PricesCheckerTimer.java
@@ -51,14 +51,11 @@ public class PricesCheckerTimer extends AbstractMTGServer {
 							try {
 								List<MagicPrice> list = prov.getPrice(alert.getCard().getCurrentSet(),alert.getCard());
 								for (MagicPrice p : list) {
-									if (p.getValue() <= alert.getPrice()&& p.getValue() > Double.parseDouble(MTGControler.getInstance().get("min-price-alert"))) 
+									if (p.getValue() <= alert.getPrice()&& p.getValue() > Double.parseDouble(MTGControler.getInstance().get("min-price-alert")) && (alert.isFoil() && p.isFoil()) || (!alert.isFoil())) 
 									{
-										if((alert.isFoil() && p.isFoil()) || (!alert.isFoil()))
-										{
-											alert.getOffers().add(p);
-											okz.add(p);
-											logger.info("Found offer " + prov + ":" + alert.getCard() + " " + p.getValue()+ p.getCurrency() + " foil=" + p.isFoil());
-										}
+										alert.getOffers().add(p);
+										okz.add(p);
+										logger.info("Found offer " + prov + ":" + alert.getCard() + " " + p.getValue()+ p.getCurrency() + " foil=" + p.isFoil());
 									}
 								}
 								prov.alertDetected(okz);
