6608251e5cec9fa8f4e1250479f5131bfe861442
nicho92 <nicolas.pihen@gmail.com>
2020-10-26 20:56:07 +0100

    pom update

--- src/main/java/org/magic/services/recognition/AverageHash.java
+++ src/main/java/org/magic/services/recognition/AverageHash.java
@@ -1,131 +1,132 @@
 package org.magic.services.recognition;
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public class AverageHash {
-	private static BufferedImage gray;
-	private static Graphics g;
-	private static boolean initiated = false;
-	
-	private int width;
-	private int height;
-	private long[] digest;
-	
-	public static void init(int maxW, int maxH)
-	{
-		gray = new BufferedImage(8*maxW,8*maxH,BufferedImage.TYPE_BYTE_GRAY);
-		g = gray.getGraphics();
-		initiated = true;
-	}
-	
-	public static AverageHash avgHash(BufferedImage img, int w, int h)
-	{
-		g.drawImage(img.getScaledInstance(8*w, 8*h, Image.SCALE_FAST),0,0,null);
-		
-		AverageHash d = new AverageHash();
-		d.digest = new long[w*h];
-		d.width = w;
-		d.height = h;
-		
-		int ix= 0;
-		for(int i=0;i<w;i++)
-		{
-			for(int j=0;j<w;j++)
-			{
-				d.digest[ix] = hashChunk(gray.getSubimage(i*8, j*8, 8, 8));
-				ix++;
-			}
-		}
-		return d;
-	}
+public class AverageHash
+{
+    private static BufferedImage gray;
+    private static Graphics g;
+    private static boolean initiated = false;
 
-	private static long hashChunk(BufferedImage chunk)
-	{
-		WritableRaster r = chunk.getRaster();
-		long hash = 0;
-		int avg = 0;
-		for(int x=0;x<8;x++)
-		{
-			for(int y=0;y<8;y++)
-			{
-				avg += r.getSample(x, y, 0);
-			}
-		}
-		avg /= 64;
-		for(int x=0;x<8;x++)
-		{
-			for(int y=0;y<8;y++)
-			{
-				if(r.getSample(x, y, 0)<avg)
-				{
-					hash |= 1;
-				}
-				hash<<=1;
-			}
-		}
-		return hash;
-	}
-	
-	public double match(AverageHash b)
-	{
-		return matchingBits(b)/(64.0*width*height);
-	}
-	
-	public int matchingBits(AverageHash b)
-	{
-		if(width != b.width || height != b.height)
-		{
-			return -1;
-		}
-		int ix = width*height;
-		
-		int bits = 0;
-		for(int i=0;i<ix;i++)
-		{
-			long d1 = digest[i];
-			long d2 = b.digest[i];
-			for(int j=0;j<64;j++)
-			{
-				if((d1&1)==(d2&1))
-				{
-					bits++;
-				}
-				d1>>>=1;
-				d2>>>=1;
-			}
-		}
-		return bits;
-	}
+    private int width;
+    private int height;
+    private long[] digest;
 
-	public static boolean isInitiated() {
-		return initiated;
-	}
-	
-	public void writeOut(DataOutputStream out) throws IOException
-	{
-		out.writeInt(width);
-		out.writeInt(height);
-		for(int i=0;i<width*height;i++)
-		{
-			out.writeLong(digest[i]);
-		}
-	}
-	
-	public static AverageHash readIn(ByteBuffer buf)
-	{
-		AverageHash d = new AverageHash();
-		d.width = buf.getInt();
-		d.height = buf.getInt();
-		d.digest = new long[d.width*d.height];
-		for(int i=0;i<d.width*d.height;i++)
-		{
-			d.digest[i] = buf.getLong();
-		}
-		return d;
-	}
+    public static void init(int maxW, int maxH)
+    {
+        gray = new BufferedImage(8 * maxW, 8 * maxH, BufferedImage.TYPE_BYTE_GRAY);
+        g = gray.getGraphics();
+        initiated = true;
+    }
+
+    public static AverageHash avgHash(BufferedImage img, int w, int h)
+    {
+        g.drawImage(img.getScaledInstance(8 * w, 8 * h, Image.SCALE_FAST), 0, 0, null);
+
+        AverageHash d = new AverageHash();
+        d.digest = new long[w * h];
+        d.width = w;
+        d.height = h;
+
+        int ix = 0;
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                d.digest[ix] = hashChunk(gray.getSubimage(i * 8, j * 8, 8, 8));
+                ix++;
+            }
+        }
+        return d;
+    }
+
+    private static long hashChunk(BufferedImage chunk)
+    {
+        WritableRaster r = chunk.getRaster();
+        long hash = 0;
+        int avg = 0;
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                avg += r.getSample(x, y, 0);
+            }
+        }
+        avg /= 64;
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                if (r.getSample(x, y, 0) < avg)
+                {
+                    hash |= 1;
+                }
+                hash <<= 1;
+            }
+        }
+        return hash;
+    }
+
+    public double match(AverageHash b)
+    {
+        return matchingBits(b) / (64.0 * width * height);
+    }
+
+    public int matchingBits(AverageHash b)
+    {
+        if (width != b.width || height != b.height)
+        {
+            return -1;
+        }
+        int ix = width * height;
+
+        int bits = 0;
+        for (int i = 0; i < ix; i++)
+        {
+            long d1 = digest[i];
+            long d2 = b.digest[i];
+            for (int j = 0; j < 64; j++)
+            {
+                if ((d1 & 1) == (d2 & 1))
+                {
+                    bits++;
+                }
+                d1 >>>= 1;
+                d2 >>>= 1;
+            }
+        }
+        return bits;
+    }
+
+    public static boolean isInitiated()
+    {
+        return initiated;
+    }
+
+    public void writeOut(DataOutputStream out) throws IOException
+    {
+        out.writeInt(width);
+        out.writeInt(height);
+        for (int i = 0; i < width * height; i++)
+        {
+            out.writeLong(digest[i]);
+        }
+    }
+
+    public static AverageHash readIn(ByteBuffer buf)
+    {
+        AverageHash d = new AverageHash();
+        d.width = buf.getInt();
+        d.height = buf.getInt();
+        d.digest = new long[d.width * d.height];
+        for (int i = 0; i < d.width * d.height; i++)
+        {
+            d.digest[i] = buf.getLong();
+        }
+        return d;
+    }
 }
--- src/main/java/org/magic/services/recognition/ImageDesc.java
+++ src/main/java/org/magic/services/recognition/ImageDesc.java
@@ -1,11 +1,4 @@
 package org.magic.services.recognition;
-import java.awt.image.BufferedImage;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import org.ddogleg.struct.FastQueue;
-
 import boofcv.abst.feature.associate.AssociateDescription;
 import boofcv.abst.feature.associate.ScoreAssociation;
 import boofcv.abst.feature.detdesc.DetectDescribePoint;
@@ -17,148 +10,170 @@ import boofcv.core.image.ConvertImage;
 import boofcv.factory.feature.associate.FactoryAssociation;
 import boofcv.factory.feature.detdesc.FactoryDetectDescribe;
 import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.feature.AssociatedIndex;
 import boofcv.struct.feature.BrightFeature;
 import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.GrayU8;
+import georegression.struct.point.Point2D_F64;
+import org.ddogleg.struct.FastQueue;
+
+import java.awt.image.BufferedImage;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ImageDesc
 {
-	private static float detectThreshold = 10;
-	private static int extractRadius = 2;
-	private static int maxFeaturesPerScale = 100;
-	private static int initialSampleSize = 2;
-	private static int initialSize = 9;
-	private static int numberScalesPerOctave = 3;
-	private static int numberOfOctaves = 4;
 	
-	private static ConfigFastHessian getHessianConf()
+	private static float detectThreshold = 10;
+    private static int extractRadius = 2;
+    private static int initialSampleSize = 2;
+    private static int initialSize = 9;
+    private static int numberScalesPerOctave = 3;
+    private static int numberOfOctaves = 4;
+    private static int maxFeaturesPerScale = 50;
+    
+    
+    private static DetectDescribePoint<GrayF32, BrightFeature> detDesc =
+            FactoryDetectDescribe.surfStable(
+                    getHessianConf(),
+                    null,
+                    null,
+                    GrayF32.class
+            );
+
+    private static ConfigFastHessian getHessianConf()
 	{
 		return new ConfigFastHessian(detectThreshold, extractRadius, maxFeaturesPerScale,initialSampleSize, initialSize, numberScalesPerOctave, numberOfOctaves); 
 	}
-	
-	
-	private static DetectDescribePoint<GrayF32, BrightFeature> detDesc = FactoryDetectDescribe.surfStable(getHessianConf(), null,null, GrayF32.class);
-	private static ScoreAssociation<BrightFeature> scorer = FactoryAssociation.defaultScore(detDesc.getDescriptionType());
-	private static AssociateDescription<BrightFeature> associate = FactoryAssociation.greedy(scorer, Double.MAX_VALUE, true);
-	
-	private AverageHash hash;
-	private AverageHash flipped;
-	private FastQueue<BrightFeature> desc = UtilFeature.createQueue(detDesc,0);
-	
+    
+    
+    private static ScoreAssociation<BrightFeature> scorer =
+            FactoryAssociation.defaultScore(detDesc.getDescriptionType());
+    private static AssociateDescription<BrightFeature> associate =
+            FactoryAssociation.greedy(scorer, 8, true);
 
-	
-	public ImageDesc(BufferedImage in, BufferedImage flipin)
-	{
-		if(!AverageHash.isInitiated())
-		{
-			AverageHash.init(2, 2);
-		}
-		hash = AverageHash.avgHash(in,2,2);
-		if(flipin != null)
-		{
-			flipped = AverageHash.avgHash(flipin,2,2);
-		}
-		int[] histogram = new int[256];
-		int[] transform = new int[256];
-		
-		GrayU8 img = ConvertBufferedImage.convertFromSingle(in, null, GrayU8.class);
-		GrayU8 norm = img.createSameShape();
-		ImageStatistics.histogram(img,0,histogram);
-		EnhanceImageOps.equalize(histogram, transform);
-		EnhanceImageOps.applyTransform(img, transform, norm);
-		GrayF32 normf = new GrayF32(img.width,img.height);
-		ConvertImage.convert(norm, normf);
-		desc.reset();
-		describeImage(normf,desc);
-	}
+    private AverageHash hash;
+    private AverageHash flipped;
+    private FastQueue<BrightFeature> desc = UtilFeature.createQueue(detDesc, 0);
+    private List<Point2D_F64> points = new ArrayList<>(0);
+    private int size;
 
-	public ImageDesc(BufferedImage in)
-	{
-		this(in,null);
-	}
-	
-	public ImageDesc(FastQueue<BrightFeature> d, AverageHash h)
-	{
-		desc = d;
-		hash = h;
-	}
-	
-	public void writeOut(DataOutputStream out) throws IOException
-	{
-		writeDescOut(out,desc);
-		hash.writeOut(out);
-	}
-	
-	public void writeDescOut(DataOutputStream out, FastQueue<BrightFeature> d) throws IOException
-	{
-		out.writeInt(d.data.length);
-		for(BrightFeature ft:d.data)
-		{
-			out.writeInt(ft.value.length);
-			for(double val:ft.value)
-			{
-				out.writeDouble(val);
-			}
-		}
-	}
-	
-	public static ImageDesc readIn(ByteBuffer buf)
-	{
-		FastQueue<BrightFeature> d = readDescIn(buf,detDesc);
-		AverageHash h = AverageHash.readIn(buf);
-		return new ImageDesc(d,h);
-	}
-	
-	public static FastQueue<BrightFeature> readDescIn(ByteBuffer buf,DetectDescribePoint<GrayF32,BrightFeature> ddp)
-	{
-		FastQueue<BrightFeature> d = UtilFeature.createQueue(ddp,0);
-		int dts = buf.getInt();
-		for(int i=0;i<dts;i++)
-		{
-			int vs = buf.getInt();
-			BrightFeature f = new BrightFeature(vs);
-			double[] vls = new double[vs];
-			for(int j=0;j<vs;j++)
-			{
-				vls[j]=buf.getDouble();
-			}
-			f.set(vls);
-			d.add(f);
-		}
-		return d;
-	}
-	
-	private void describeImage(GrayF32 input, FastQueue<BrightFeature> descs) {
-		detDesc.detect(input);
-		for (int i = 0; i < detDesc.getNumberOfFeatures(); i++) {
-			descs.grow().setTo(detDesc.getDescription(i));
-		}
-	}
+    public ImageDesc(BufferedImage in, BufferedImage flipin)
+    {
+        if (!AverageHash.isInitiated())
+        {
+            AverageHash.init(2, 2);
+        }
+        hash = AverageHash.avgHash(in, 2, 2);
+        if (flipin != null)
+        {
+            flipped = AverageHash.avgHash(flipin, 2, 2);
+        }
+        int[] histogram = new int[256];
+        int[] transform = new int[256];
+        GrayU8 img = ConvertBufferedImage.convertFromSingle(in, null, GrayU8.class);
+        GrayU8 norm = img.createSameShape();
+        ImageStatistics.histogram(img, 0, histogram);
+        EnhanceImageOps.equalize(histogram, transform);
+        EnhanceImageOps.applyTransform(img, transform, norm);
+        GrayF32 normf = new GrayF32(img.width, img.height);
+        ConvertImage.convert(norm, normf);
+        desc.reset();
+        size = describeImage(normf, desc, points);
+    }
 
-	public double compareSURF(ImageDesc i2) {
-		associate.setSource(desc);
-		associate.setDestination(i2.desc);
-		associate.associate();
-		
-		double max = Math.max(desc.size(), i2.desc.size());
+    public ImageDesc(BufferedImage in)
+    {
+        this(in, null);
+    }
 
-		double score = 0;
-		for (int i=0;i<associate.getMatches().size();i++)
-		{
-			score += 1 - associate.getMatches().data[i].fitScore;
-		}
-		
-		return score/max;
-	}
-	
-	public double compareHash(ImageDesc i2)
-	{
-		return hash.match(i2.hash);
-	}
+    public ImageDesc(FastQueue<BrightFeature> d, List<Point2D_F64> p, AverageHash h)
+    {
+        desc = d;
+        hash = h;
+        points = p;
+        size = p.size();
+    }
 
-	public double compareHashWithFlip(ImageDesc i2)
-	{
-		return Math.max(hash.match(i2.hash), flipped.match(i2.hash));
-	}
+    public void writeOut(DataOutputStream out) throws IOException
+    {
+        out.writeInt(size);
+        for (int i = 0; i < size; i++)
+        {
+            BrightFeature f = desc.get(i);
+            for (double val : f.value)
+            {
+                out.writeDouble(val);
+            }
+            Point2D_F64 pt = points.get(i);
+            out.writeDouble(pt.x);
+            out.writeDouble(pt.y);
+        }
+        hash.writeOut(out);
+    }
+
+    public static ImageDesc readIn(ByteBuffer buf)
+    {
+        int size = buf.getInt();
+        List<Point2D_F64> points = new ArrayList<>(size);
+        FastQueue<BrightFeature> descs = UtilFeature.createQueue(detDesc, size);
+        for (int i = 0; i < size; i++)
+        {
+            BrightFeature f = detDesc.createDescription();
+            for (int j = 0; j < f.size(); j++)
+            {
+                f.value[j] = buf.getDouble();
+            }
+            descs.add(f);
+            points.add(new Point2D_F64(
+                    buf.getDouble(), buf.getDouble()
+            ));
+        }
+        AverageHash hash = AverageHash.readIn(buf);
+        return new ImageDesc(descs, points, hash);
+    }
+
+    public static int describeImage(GrayF32 input, FastQueue<BrightFeature> descs, List<Point2D_F64> points)
+    {
+        detDesc.detect(input);
+        int size = detDesc.getNumberOfFeatures();
+        for (int i = 0; i < size; i++)
+        {
+            descs.grow().setTo(detDesc.getDescription(i));
+            points.add(detDesc.getLocation(i));
+        }
+        return size;
+    }
+
+    public double compareSURF(ImageDesc i2)
+    {
+        associate.setSource(desc);
+        associate.setDestination(i2.desc);
+        associate.associate();
+
+        double max = Math.max(desc.size(), i2.desc.size());
+        FastQueue<AssociatedIndex> matches = associate.getMatches();
+        double score = 0;
+        for (int i = 0; i < matches.size(); i++)
+        {
+            AssociatedIndex match = matches.get(i);
+            score += 1 - match.fitScore;
+        }
+        score = score / max;
+        return score;
+    }
+
+    public double compareHash(ImageDesc i2)
+    {
+        return hash.match(i2.hash);
+    }
+
+    public double compareHashWithFlip(ImageDesc i2)
+    {
+        return Math.max(hash.match(i2.hash), flipped.match(i2.hash));
+    }
 }
