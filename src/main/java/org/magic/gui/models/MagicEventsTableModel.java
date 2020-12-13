a606bebacf9bf3f082a325831d1a757ecd2edc85
nicho92 <nicolas.pihen@gmail.com>
2020-12-13 22:37:33 +0100

    update model

--- src/main/java/org/magic/gui/models/MagicEventsTableModel.java
+++ src/main/java/org/magic/gui/models/MagicEventsTableModel.java
@@ -1,6 +1,10 @@
 package org.magic.gui.models;
 
+import java.sql.Date;
+
 import org.magic.api.beans.MagicEvent;
+import org.magic.api.beans.MagicEvent.EVENT_FORMAT;
+import org.magic.api.beans.MagicEvent.ROUNDS;
 import org.magic.gui.abstracts.GenericTableModel;
 
 public class MagicEventsTableModel extends GenericTableModel<MagicEvent> {
@@ -13,6 +17,20 @@ public class MagicEventsTableModel extends GenericTableModel<MagicEvent> {
 	
 	public MagicEventsTableModel() {
 		setColumns("Title","Date","Format","Round","Winning round","Round time");
+	}
+	
+	
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch (columnIndex) {
+			case 0: return MagicEvent.class;
+			case 1 : return Date.class;
+			case 2 : return EVENT_FORMAT.class;
+			case 3 : return ROUNDS.class;
+			case 4 : return Integer.class;
+			case 5 : return Integer.class;
+			default: return super.getColumnClass(columnIndex);
+		}
 	}
 	
 	
