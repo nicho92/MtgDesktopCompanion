d88d8acbb31bb8a86fc76242d9a3a068ee5e4cb9
Nicho <nicolas.pihen@gmail.com>
2021-01-04 17:40:16 +0100

    mtgstand update

--- src/main/java/org/beta/MTGStandExport.java
+++ src/main/java/org/beta/MTGStandExport.java
@@ -2,7 +2,12 @@ package org.beta;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 
+import org.magic.api.beans.MagicCard;
+import org.magic.api.beans.MagicCardStock;
 import org.magic.api.beans.MagicDeck;
 import org.magic.api.interfaces.abstracts.AbstractFormattedFileCardExport;
 
@@ -10,21 +15,63 @@ public class MTGStandExport extends AbstractFormattedFileCardExport {
 
 	@Override
 	public String getFileExtension() {
-		// TODO Auto-generated method stub
-		return null;
+		return ".csv";
 	}
 
 	@Override
 	public void exportDeck(MagicDeck deck, File dest) throws IOException {
-		// TODO Auto-generated method stub
+		exportStock(importFromDeck(deck), dest);
 
 	}
+	
 
 	@Override
 	public MagicDeck importDeck(String f, String name) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		MagicDeck d = new MagicDeck();
+		d.setName(name);
+		
+		for(MagicCardStock st : importStock(f))
+		{
+			d.getMain().put(st.getMagicCard(), st.getQte());
+		}
+		return d;
 	}
+	
+	
+	@Override
+	public void exportStock(List<MagicCardStock> stock, File f) throws IOException {
+		// TODO Auto-generated method stub
+		super.exportStock(stock, f);
+	}
+	
+	@Override
+	public List<MagicCardStock> importStock(String content) throws IOException {
+		List<MagicCardStock> ret = new ArrayList<>();
+		for(Matcher m : matches(content, true))
+		{
+			MagicCard mc = parseMatcherWithGroup(m, 1, 4, true, FORMAT_SEARCH.ID, FORMAT_SEARCH.NAME);
+			
+			if(mc !=null )
+			{
+				
+				MagicCardStock st = new MagicCardStock();
+				st.setMagicCard(mc);
+				st.setQte(Integer.parseInt(m.group(2)));
+				st.setLanguage(m.group(6));
+				st.setFoil(m.group(7).equals("1"));
+				st.setComment(m.group(10));
+				
+				ret.add(st);
+				notify(mc);
+			}
+			
+			
+			
+		}
+		
+		return ret;
+	}
+	
 
 	@Override
 	public String getName() {
@@ -33,26 +80,22 @@ public class MTGStandExport extends AbstractFormattedFileCardExport {
 
 	@Override
 	protected boolean skipFirstLine() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	protected String[] skipLinesStartWith() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[] {" "};
 	}
 
 	@Override
 	protected String getStringPattern() {
-		// TODO Auto-generated method stub
-		return null;
+		return "\"(.*?)\",(\\d+),(.*?),(.*?),(\\d+),(.*?),(\\d+),\"(.*?)\",(.*?),(.*?)$";
 	}
 
 	@Override
 	protected String getSeparator() {
-		// TODO Auto-generated method stub
-		return null;
+		return ",";
 	}
 
 }
