b7aa02f2932fe43e9240d5603316e4063b578812
nicho92 <nicolas.pihen@gmail.com>
2021-04-14 23:25:09 +0200

    mtg price parser

--- src/main/java/org/beta/MTGJsonPricer.java
+++ src/main/java/org/beta/MTGJsonPricer.java
@@ -5,64 +5,98 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
-import org.magic.api.beans.HistoryPrice;
 import org.magic.services.MTGLogger;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
-
 
 public class MTGJsonPricer {
 	
 	protected Logger logger = MTGLogger.getLogger(this.getClass());
 
-	public static void main(String[] args) throws JsonParseException, IOException {
-		File f = new File("C:\\Users\\Pihen\\Downloads\\AllPrices.json");
+	public static void main(String[] args) throws IOException {
+	//	File f = new File("d:\\Téléchargements\\AllPrices.json");
+		File f = new File("C:\\Users\\Nicolas\\OneDrive - Ville de Boulogne-Billancourt\\test.json");
 		new MTGJsonPricer(f);
 	}
 	
-	public MTGJsonPricer(File f) throws IOException {
 
+	public MTGJsonPricer(File f) throws IOException 
+	{
 		Gson gson = new GsonBuilder().create();
-		
 		try(JsonReader reader = new JsonReader(new FileReader(f)))
 		{
 			reader.beginObject();
 			
 			if("meta".equalsIgnoreCase(reader.nextName()))
-				logger.debug(gson.fromJson(reader, Meta.class));
+				logger.debug("META" + gson.fromJson(reader, Meta.class));
 			
 			if("data".equalsIgnoreCase(reader.nextName()))
 			{
-				while (reader.hasNext()) {
-					JsonToken t = reader.peek();
-					switch(t)
-					{
-					case BEGIN_OBJECT:reader.beginObject();break;
-		            case END_OBJECT:reader.endObject();break;
-		            case BEGIN_ARRAY:reader.beginArray();break;
-		            case END_ARRAY:reader.endArray();break;
-		            case NAME:System.out.println(reader.nextName());break;
-					case BOOLEAN:System.out.println(reader.nextBoolean());break;
-					case END_DOCUMENT:break;
-					case NULL:reader.nextNull();break;
-					case NUMBER:System.out.println(reader.nextDouble());break;
-					case STRING:System.out.println(reader.nextString());break;
-					default:break;
-						
-					}
-					
-						
-				}
-			}		
+				reader.beginObject();
+				buildPrice(reader);
+			}
 		}
+		
+		
+		
+		
+		
+	}
 
-
-}
+	private void buildPrice(JsonReader reader) throws IOException {
+		while(reader.hasNext())
+		{
+			logger.debug("Card ID = " + reader.nextName());
+			reader.beginObject();
+			while(reader.hasNext())
+			{
+				logger.debug("support="+reader.nextName());
+				reader.beginObject();
+				while(reader.hasNext())
+				{
+					logger.debug("provider="+reader.nextName());
+					reader.beginObject();
+					while(reader.hasNext())
+					{
+						logger.debug("stock="+reader.nextName());
+						reader.beginObject();
+						while(reader.hasNext())
+						{
+							logger.debug("format="+reader.nextName());
+							reader.beginObject();
+							while(reader.hasNext())
+							{
+								if(reader.peek()==JsonToken.NAME)
+								{ 
+									String name =reader.nextName();
+									logger.debug("currency=" + reader.nextString());
+								}
+								else 
+								{
+									reader.beginObject();
+									while(reader.hasNext())
+									{
+										logger.debug(reader.nextName());
+										logger.debug(reader.nextDouble());
+									}
+									reader.endObject();
+								}
+							}
+							reader.endObject();
+						}
+						reader.endObject();
+					}
+					reader.endObject();
+				}
+				reader.endObject();
+			}
+			reader.endObject();
+		}
+		reader.endObject();
+	}
 
 }
 
@@ -88,5 +122,11 @@ class Meta
 	public void setVersion(String version) {
 		this.version = version;
 	}
+} 
+
+
+class MTGJsonPrice
+{
+	String mtgjsonId;
 }
 
