a14185b17a619db8a8585b9c02337c07607e66dc
nicho92 <nicolas.pihen@gmail.com>
2021-05-20 23:27:13 +0200

    Woocommerce import orders plugin

--- src/main/java/org/magic/api/shopping/impl/WooCommerceShopper.java
+++ src/main/java/org/magic/api/shopping/impl/WooCommerceShopper.java
@@ -1,7 +1,6 @@
 package org.magic.api.shopping.impl;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,12 +12,12 @@ import org.magic.api.interfaces.abstracts.AbstractMagicShopper;
 import org.magic.tools.WooCommerceTools;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.icoderman.woocommerce.EndpointBaseType;
 import com.icoderman.woocommerce.WooCommerce;
 
 public class WooCommerceShopper extends AbstractMagicShopper{
 
+	private static final String STATUS = "STATUS";
 	private static final String WEBSITE = "WEBSITE";
 	private static final String CONSUMER_KEY = "CONSUMER_KEY";
 	private static final String CONSUMER_SECRET = "CONSUMER_SECRET";
@@ -29,6 +28,7 @@ public class WooCommerceShopper extends AbstractMagicShopper{
 		setProperty(WEBSITE, "https://mysite.fr/");
 		setProperty(CONSUMER_KEY, "");
 		setProperty(CONSUMER_SECRET, "");
+		setProperty(STATUS,"any");
 	}
 	
 
@@ -54,28 +54,26 @@ public class WooCommerceShopper extends AbstractMagicShopper{
 		
 		Map<String, String> parameters = new HashMap<>();
 						    parameters.put("per_page", "100");
-						    parameters.put("status", "completed");
+						    parameters.put("status", getString(STATUS));
 						    
 		List<JsonElement> ret = wooCommerce.getAll(EndpointBaseType.ORDERS.getValue(),parameters);
-		System.out.println(ret);
-		
-		
 		for(JsonElement el : ret)
 		{
-			JsonObject obj = el.getAsJsonObject();
+			var obj = el.getAsJsonObject();
 			
 			for(JsonElement item : obj.get("line_items").getAsJsonArray())
 			{
-				JsonObject itemObj = item.getAsJsonObject();
+				var itemObj = item.getAsJsonObject();
 				var oe = new OrderEntry();
 				   oe.setCurrency(obj.get("currency").getAsString());
 				   oe.setIdTransation(obj.get("id").getAsString());
-				   oe.setTransactionDate(WooCommerceTools.toDate(obj.get("date_completed").getAsString()));
+				   oe.setTransactionDate(WooCommerceTools.toDate(obj.get("date_created").getAsString()));
 				   oe.setDescription(itemObj.get("name").getAsString());
-				   oe.setSeller(getString(WEBSITE));
+				   oe.setSeller(obj.get("billing").getAsJsonObject().get("last_name").getAsString());
 				   oe.setSource(getString(WEBSITE));
 				   oe.setItemPrice(itemObj.get("price").getAsDouble());
 				   oe.setTypeTransaction(TYPE_TRANSACTION.BUY);
+				   oe.setShippingPrice(obj.get("shipping_total").getAsDouble());
 				   list.add(oe);
 			}
 		}
