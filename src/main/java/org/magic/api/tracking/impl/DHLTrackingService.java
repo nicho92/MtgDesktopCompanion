5da779b5e0b2db77e9f061caa3789470eb6fc05c
nicho92 <nicolas.pihen@gmail.com>
2021-05-29 12:09:54 +0200

    dhl api

---
+++ src/main/java/org/magic/api/tracking/impl/DHLTrackingService.java
@@ -0,0 +1,29 @@
+package org.magic.api.tracking.impl;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.magic.api.beans.Tracking;
+import org.magic.api.interfaces.abstracts.AbstractTrackingService;
+
+public class DHLTrackingService extends AbstractTrackingService {
+
+	@Override
+	public Tracking track(String number) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL trackUriFor(String number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
