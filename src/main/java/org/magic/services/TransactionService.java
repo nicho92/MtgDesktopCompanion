8e9035e7ed89bc598cde1326c12b8f9d3c2fcd27
Pihen <Pihen@UC5104.mairie-boulogne-billancourt.fr>
2021-06-28 15:28:27 +0200

    bugfix

--- src/main/java/org/magic/services/TransactionService.java
+++ src/main/java/org/magic/services/TransactionService.java
@@ -175,7 +175,7 @@ public class TransactionService
 					   transactionItem.setComment("");
 					   stock.setQte(stock.getQte()-transactionItem.getQte());
 					   stock.setUpdated(true);
-					   accepteds.add(stock);
+					   accepteds.add(transactionItem);
 				}
 		}
 		
