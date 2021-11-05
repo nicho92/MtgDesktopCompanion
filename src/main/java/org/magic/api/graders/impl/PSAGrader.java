2f20ade0fbf687e26d2cce5e82e2c0387f5fb335
Pihen <Pihen@UC5104.mairie-boulogne-billancourt.fr>
2021-11-05 16:30:08 +0100

    PSA Gradding is now enabled

--- src/main/java/org/magic/api/graders/impl/PSAGrader.java
+++ src/main/java/org/magic/api/graders/impl/PSAGrader.java
@@ -2,9 +2,14 @@ package org.magic.api.graders.impl;
 
 import java.io.IOException;
 
+import javax.script.ScriptException;
+
 import org.magic.api.beans.Grading;
+import org.magic.api.beans.enums.EnumCondition;
 import org.magic.api.interfaces.abstracts.AbstractGradersProvider;
+import org.magic.tools.CloudFlareAuthorizer;
 import org.magic.tools.UITools;
+import org.magic.tools.URLTools;
 
 public class PSAGrader extends AbstractGradersProvider {
 
@@ -15,9 +20,50 @@ public class PSAGrader extends AbstractGradersProvider {
 	
 	@Override
 	public Grading loadGrading(String identifier) throws IOException {
-		logger.error("Blocked by captcha protection");
-		UITools.browse(getWebSite()+"/cert/"+identifier);
+		var authorizer = new CloudFlareAuthorizer();
+    	try {
+			var ret = authorizer.getAuthorizationResult(getWebSite()+"/cert/"+identifier);
+			var doc = URLTools.toHtml(ret);
+			
+			
+			for(var it : doc.select("table tr"))
+			{
+				if(it.getElementsByTag("th").first().text().equals("Grade"))
+				{
+					var result = it.getElementsByTag("td").html();
+					logger.debug("result : " + result);
+				    var  condition=result.split(" ")[0].trim();
+					var note = UITools.parseDouble (result.split(" ")[1].trim());
+					
+					var g = new Grading();
+					g.setNumberID(identifier);
+					g.setUrlInfo(getWebSite()+"/cert/"+identifier);
+					g.setGraderName(getName());
+					g.setGrade(parseCondition(condition)); 
+					g.setGradeNote(note);
+					
+					return g;
+					
+
+					
+				}
+			}
+			
+			
+		} catch (IOException | ScriptException e) {
+			throw new IOException(e);
+		}
 		return null;
+	}
+
+	private EnumCondition parseCondition(String condition) {
+		switch (condition)
+		{
+			case "NM-MT": return EnumCondition.NEAR_MINT;
+			case "PR" : return EnumCondition.POOR;
+			case "MINT": return EnumCondition.MINT;
+			default : logger.warn(condition  + " is unknow"); return EnumCondition.NEAR_MINT;
+		}
 	}
 
 	@Override
