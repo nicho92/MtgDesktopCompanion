40bab4e95b8214c6807dd99e23d5b8f3be28970f
Pihen <Pihen@UC5104.mairie-boulogne-billancourt.fr>
2021-11-05 11:05:21 +0100

    gameclient fix

--- src/main/java/org/magic/api/main/GameClient.java
+++ src/main/java/org/magic/api/main/GameClient.java
@@ -2,7 +2,10 @@ package org.magic.api.main;
 
 import static org.magic.tools.MTG.getEnabledPlugin;
 
+import java.sql.SQLException;
+
 import org.magic.api.interfaces.MTGCardsProvider;
+import org.magic.api.interfaces.MTGDao;
 import org.magic.gui.GameGUI;
 import org.magic.gui.abstracts.MTGUIComponent;
 import org.magic.services.MTGControler;
@@ -10,9 +13,11 @@ import org.magic.services.threads.ThreadManager;
 
 public class GameClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		MTGControler.getInstance();
+		
 		getEnabledPlugin(MTGCardsProvider.class).init();
+		getEnabledPlugin(MTGDao.class).init();
 		ThreadManager.getInstance().invokeLater(() -> MTGUIComponent.createJFrame(new GameGUI(), true, true).setVisible(true));
 	}
 	
