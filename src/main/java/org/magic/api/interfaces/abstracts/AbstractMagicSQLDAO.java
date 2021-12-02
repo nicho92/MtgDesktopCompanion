542b5eab454ffccaba40ca2473633f037c3bb439
Pihen <Pihen@UC5104.mairie-boulogne-billancourt.fr>
2021-12-02 18:46:28 +0100

    dao sql update announces

--- src/main/java/org/magic/api/interfaces/abstracts/AbstractMagicSQLDAO.java
+++ src/main/java/org/magic/api/interfaces/abstracts/AbstractMagicSQLDAO.java
@@ -278,26 +278,30 @@ public abstract class AbstractMagicSQLDAO extends AbstractMagicDAO {
 					pst.setInt(10, n.getContact().getId());
 					var ret = pst.executeUpdate();
 					n.setId(ret);
-					
 					logger.debug(n  +" created");
-					n.setUpdated(false);
 					
-		
 				}
-				
 		}
-		
+		else if(n.isUpdated())
+			{
+				try (var c = pool.getConnection(); PreparedStatement pst = c.prepareStatement("UPDATE announces SET startDate = ?, endDate =?, expireDate =?', title=?,  description = ?, total = ?, currency = ?, stocksItem = ?, typeAnnounce = ?, fk_idcontact = ? WHERE id = ?;")) 
+				{
+					pst.setDate(1,new Date(n.getStartDate().getTime()));
+					pst.setDate(2,new Date(n.getEndDate().getTime()));
+					pst.setDate(3, new Date(n.getExpirationDate().getTime()));
+					pst.setString(4, n.getTitle());
+					pst.setString(5, n.getDescription());
+					pst.setDouble(6, n.getTotalPrice());
+					pst.setString(7, n.getCurrency().getCurrencyCode());
+					storeTransactionItems(pst,8, n.getItems());
+					pst.setString(9, n.getType().name());
+					pst.setInt(10, n.getContact().getId());
+				}	
+				
+			}
+		n.setUpdated(false);		
 		return n.getId();
-		
-//		else if(n.isUpdated())
-//		{
-//			try (var c = pool.getConnection(); PreparedStatement pst = c.prepareStatement("UPDATE conversionsItems SET name = ?, lang = ?, source = ?, inputId = ?, destination = ?, outputId = ? WHERE id = ?")) 
-//			{
-//				
-//				n.setUpdated(false);	
-//			}	
-//			
-//		}
+
 		
 	}
 	@Override
