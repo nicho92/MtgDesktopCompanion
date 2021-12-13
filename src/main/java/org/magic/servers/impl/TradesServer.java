b3651830af2dc4f0bfca26ce12a99a4fb6a5fa37
Pihen <Pihen@UC5104.mairie-boulogne-billancourt.fr>
2021-12-13 12:53:58 +0100

    update source html

--- src/main/java/org/magic/servers/impl/TradesServer.java
+++ src/main/java/org/magic/servers/impl/TradesServer.java
@@ -1,21 +1,9 @@
 package org.magic.servers.impl;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.swing.Icon;
 
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.magic.api.interfaces.abstracts.AbstractWebServer;
 import org.magic.services.MTGConstants;
-import org.magic.services.MTGControler;
-import org.magic.services.network.URLTools;
-import org.magic.tools.FileTools;
 
 public class TradesServer extends AbstractWebServer {
 	@Override
@@ -28,37 +16,6 @@ public class TradesServer extends AbstractWebServer {
 		return "WebTraders";
 	}
 
-	@Override
-	public void exportWeb(File dest) throws IOException
-	{
-		super.exportWeb(dest);
-		FileTools.saveFile(Paths.get(dest.getAbsolutePath(),"css","extra.css").toFile(), MTGControler.getInstance().getWebConfig().getExtraCss());
-	}
-	
-	@Override
-	public void extraConfig() {
-				var holderCss = new ServletHolder("extraCssServlet", new DefaultServlet() {
-					private static final long serialVersionUID = 1L;
-					@Override
-					protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-						  response.setContentType(URLTools.HEADER_CSS+";charset="+MTGConstants.DEFAULT_ENCODING);
-						  response.setStatus(HttpServletResponse.SC_OK);
-						  try { 
-							  response.getWriter().println(MTGControler.getInstance().getWebConfig().getExtraCss());  
-						  }
-						  catch(Exception e)
-						  {
-							  response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-							  logger.error(e);
-						  }
-					}
-				});
-			
-				ctx.addServlet(holderCss,"/css/extra.css");
-		
-		
-		
-	}
 	
 	
 	@Override
