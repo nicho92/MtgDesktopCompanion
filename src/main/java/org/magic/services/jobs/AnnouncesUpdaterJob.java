30b4ca583930f691c41e5871c11d2998b4df5920
nicho92 <nicolas.pihen@gmail.com>
2021-12-15 15:41:37 +0100

    add qwartz job for announces updates

---
+++ src/main/java/org/magic/services/jobs/AnnouncesUpdaterJob.java
@@ -0,0 +1,56 @@
+package org.magic.services.jobs;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.magic.api.beans.Announce.STATUS;
+import org.magic.api.interfaces.MTGDao;
+import org.magic.services.MTGLogger;
+import org.magic.tools.MTG;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class AnnouncesUpdaterJob implements Job {
+	protected Logger logger = MTGLogger.getLogger(this.getClass());
+
+	
+	
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		try {
+			var list = MTG.getEnabledPlugin(MTGDao.class).listAnnounces(true);
+			
+			list.stream().filter(a->a.getEndDate().before(new Date()) && a.getStatus()==STATUS.ACTIVE).toList().forEach(a->{
+				logger.debug("Found " + a + " is expired at " + a.getEndDate());
+				a.setStatus(STATUS.EXPIRED);
+				try {
+					MTG.getEnabledPlugin(MTGDao.class).saveOrUpdateAnnounce(a);
+				} catch (SQLException e) {
+					logger.error("can't update " + a +" " + e);
+				}
+				
+			});
+			
+			list.stream().filter(a->a.getStartDate().after(new Date()) && a.getStatus()==STATUS.SOON).toList().forEach(a->{
+				logger.debug("Found " + a + " is now online since " + a.getStartDate());
+				a.setStatus(STATUS.ACTIVE);
+				try {
+					MTG.getEnabledPlugin(MTGDao.class).saveOrUpdateAnnounce(a);
+				} catch (SQLException e) {
+					logger.error("can't update " + a +" " + e);
+				}
+				
+			});
+			
+			
+			
+			
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+
+	}
+
+}
