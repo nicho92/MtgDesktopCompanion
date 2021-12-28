e9a167730c8ce2869b737c10d54aa20f58c21451
nicho92 <nicolas.pihen@gmail.com>
2021-12-28 14:49:15 +0100

    source clean

--- src/main/java/org/magic/api/interfaces/MTGPictureCache.java
+++ src/main/java/org/magic/api/interfaces/MTGPictureCache.java
@@ -1,14 +1,11 @@
 package org.magic.api.interfaces;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import org.magic.api.beans.MagicCard;
 
 public interface MTGPictureCache extends MTGCache<MagicCard, BufferedImage> {
 
-	public BufferedImage getItem(MagicCard mc);
 
-	public void put(BufferedImage im, MagicCard mc) throws IOException;
 
 }
