d2d6cfc0140f4131bad53696675e91a59e236b5b
nicho92 <nicolas.pihen@gmail.com>
2022-02-13 15:08:25 +0100

    source clean

--- src/main/java/org/magic/gui/components/TechnicalMonitorPanel.java
+++ src/main/java/org/magic/gui/components/TechnicalMonitorPanel.java
@@ -2,7 +2,6 @@ package org.magic.gui.components;
 import static org.magic.tools.MTG.capitalize;
 
 import java.awt.BorderLayout;
-import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
