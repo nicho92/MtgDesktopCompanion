21770b099dc8c54169b0a14d6b46a705bddd2be7
nicho92 <nicolas.pihen@gmail.com>
2022-03-06 19:20:03 +0100

    decks page

--- src/main/resources/web/web-ui2/dist/js/mtgcompanion-ui.js
+++ src/main/resources/web/web-ui2/dist/js/mtgcompanion-ui.js
@@ -54,3 +54,84 @@ function mtgtooltipStock(element)
     });
 	
 }
+
+
+function addCard(idCard,to,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/cards/add/"+to+"/"+idCard
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+	
+}
+
+function addCardtoDefaultLibrary(idCard,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/cards/add/"+idCard
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+	
+}
+
+function moveCard(idCard,from, to,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/cards/move/"+from+"/"+to+"/"+idCard
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+}
+
+
+function addAlert(idCard,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/alerts/add/"+idCard
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+}
+
+
+function addStock(idCard,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/stock/add/"+idCard
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+
+}
+
+function addCollection(name,callback)
+{
+	$.ajax({
+		type: 'PUT',
+	    url: restserver+"/collections/add/"+name
+   		 }).done(function(data) {
+   			callback();
+   		 }).fail(function(data,status,error) {
+   			alert(JSON.stringify(data) + " " + error);
+   		 });
+
+}
+
+
