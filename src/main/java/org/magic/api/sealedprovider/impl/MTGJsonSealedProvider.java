a737242005f1ace2ff80463cbed8e7b20cf6889c
Nicho <nicolas.pihen@gmail.com>
2023-03-10 09:36:07 +0100

    Update MTGJsonSealedProvider.java

--- src/main/java/org/magic/api/sealedprovider/impl/MTGJsonSealedProvider.java
+++ src/main/java/org/magic/api/sealedprovider/impl/MTGJsonSealedProvider.java
@@ -1,11 +1,13 @@
 package org.magic.api.sealedprovider.impl;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.magic.api.beans.MTGSealedProduct;
 import org.magic.api.beans.MagicEdition;
+import org.magic.api.beans.enums.EnumItems;
 import org.magic.api.interfaces.MTGCardsProvider;
 import org.magic.api.interfaces.abstracts.AbstractSealedProvider;
 import org.magic.api.providers.impl.Mtgjson5Provider;
@@ -13,35 +15,53 @@ import org.magic.api.sealedprovider.impl.MTGCompanionSealedProvider.LOGO;
 import org.magic.services.tools.MTG;
 
 public class MTGJsonSealedProvider extends AbstractSealedProvider {
-
 	
-	List<MTGSealedProduct> products;
+	private List<MTGSealedProduct> products;
 	
-	private void init()
+	private void init() throws IOException
 	{
 		if(products==null)
 		{
-			
 			Mtgjson5Provider prov = null;
 			
 			if(MTG.getEnabledPlugin(MTGCardsProvider.class).getName().equals("MTGJson5"))
 			{
-				prov=((Mtgjson5Provider)MTG.getEnabledPlugin(MTGCardsProvider.class));	
+				prov=((Mtgjson5Provider)MTG.getEnabledPlugin(MTGCardsProvider.class));
+				prov.listEditions();
 			}
 			else
 			{
+				logger.debug("Loading mtgjson5 provider");
 				prov = new Mtgjson5Provider();
 				prov.init();
+				prov.listEditions();
 			}
 			
-			products = prov.getSealeds().stream().toList();
+			products = new ArrayList<>(prov.getSealeds());
+			
+			
+				for(MagicEdition ed : products.stream().map(s->s.getEdition()).distinct().toList())
+				{
+					var p = new MTGSealedProduct();
+						p.setTypeProduct(EnumItems.SET);
+						p.setEdition(ed);
+						p.setLang("en");
+						p.setNum(1);
+						
+					products.add(p);
+				}
 		}
 	}
 	
 	
 	@Override
 	public List<MTGSealedProduct> getItemsFor(MagicEdition me) {
-		init();
+		try {
+			init();
+		} catch (IOException e) {
+			logger.error("Error Loading list {}",e);
+			return new ArrayList<>();
+		}
 		return products.stream().filter(mts->mts.getEdition().getId().equals(me.getId())).toList();
 	}
 
