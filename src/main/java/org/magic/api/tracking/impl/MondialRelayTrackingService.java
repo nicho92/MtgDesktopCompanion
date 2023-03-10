52195b23d95d2e3c7bee71fb3e1dc2824f0171e8
Nicho <nicolas.pihen@gmail.com>
2023-03-10 17:05:06 +0100

    Update MondialRelayTrackingService.java

--- src/main/java/org/magic/api/tracking/impl/MondialRelayTrackingService.java
+++ src/main/java/org/magic/api/tracking/impl/MondialRelayTrackingService.java
@@ -3,14 +3,38 @@ package org.magic.api.tracking.impl;
 import java.io.IOException;
 import java.util.List;
 
+import org.dajlab.mondialrelayapi.IMondialRelayService;
+import org.dajlab.mondialrelayapi.MondialRelayServiceImpl;
+import org.dajlab.mondialrelayapi.vo.MRException;
 import org.magic.api.beans.shop.Tracking;
+import org.magic.api.beans.shop.TrackingStep;
 import org.magic.api.interfaces.abstracts.AbstractTrackingService;
 
 public class MondialRelayTrackingService extends AbstractTrackingService {
 
 	@Override
 	public Tracking track(String number) throws IOException {
-		return new Tracking();
+		var track = new Tracking();
+		
+		try {
+			IMondialRelayService service = new MondialRelayServiceImpl(getAuthenticator().get("CODE_ENSEIGNE"), getAuthenticator().get("PRIVATE_KEY"));
+			var mrSuivi = service.getSuivi(number);
+				track.setNumber(number);
+			
+			mrSuivi.getEvenements().forEach(ev->{
+				var ts = new TrackingStep();
+					ts.setDateStep(ev.getDate().getTime());
+					ts.setDescriptionStep(ev.getLibelle());
+				
+				track.getSteps().add(ts);
+			});
+			return track;
+		} catch (MRException e) {
+			throw new IOException(e);
+		}
+		
+		
+		
 	}
 
 	@Override
