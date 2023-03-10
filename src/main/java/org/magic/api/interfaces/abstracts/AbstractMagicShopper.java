f5089878725bb55e771678a1c42ed9e6429af1cc
Nicho <nicolas.pihen@gmail.com>
2023-03-10 14:36:43 +0100

    Update AbstractMagicShopper.java

--- src/main/java/org/magic/api/interfaces/abstracts/AbstractMagicShopper.java
+++ src/main/java/org/magic/api/interfaces/abstracts/AbstractMagicShopper.java
@@ -3,7 +3,7 @@ package org.magic.api.interfaces.abstracts;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Currency;
 import org.magic.api.beans.enums.TransactionDirection;
 import org.magic.api.beans.shop.Contact;
 import org.magic.api.beans.shop.Transaction;
