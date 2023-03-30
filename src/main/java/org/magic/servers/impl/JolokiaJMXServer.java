e1a1f363a1cb40b4df104e0416e508a656ce0d45
Nicho <nicolas.pihen@gmail.com>
2023-03-30 10:03:30 +0200

    Add files via upload

---
+++ src/main/java/org/magic/servers/impl/JolokiaJMXServer.java
@@ -0,0 +1,77 @@
+package org.magic.servers.impl;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jolokia.jvmagent.JolokiaServer;
+import org.jolokia.jvmagent.JolokiaServerConfig;
+import org.magic.api.interfaces.abstracts.AbstractMTGServer;
+
+public class JolokiaJMXServer extends AbstractMTGServer {
+
+	JolokiaServer serv;
+	private boolean started=false;
+	
+	private void init() throws IOException
+	{
+		var map = new HashMap<String,String>();
+		map.put("host", getString("HOST"));
+		map.put("port", getString("PORT"));
+		map.put("agentContext", getString("CONTEXT"));
+		map.put("user",getString("USER"));
+		map.put("password",getString("PASS"));
+	
+	var jconf = new JolokiaServerConfig(map);
+	
+	serv = new JolokiaServer(jconf, true);
+	}
+	
+	@Override
+	public Map<String, String> getDefaultAttributes() {
+		var m = new HashMap<String,String>();
+		m.put("HOST", "127.0.0.1");
+		m.put("PORT", "8082");
+		m.put("CONTEXT", "/");
+		m.put("USER", "jolokia");
+		m.put("PASS", "jolokia");
+		
+		return m;
+	}
+	
+	
+	@Override
+	public void start() throws IOException {
+		init();
+		serv.start();
+	}
+
+	@Override
+	public void stop() throws IOException {
+		serv.stop();
+		started=false;
+	}
+
+	@Override
+	public boolean isAlive() {
+		return started;
+	}
+
+	@Override
+	public boolean isAutostart() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "Jolokaria";
+	}
+
+}
