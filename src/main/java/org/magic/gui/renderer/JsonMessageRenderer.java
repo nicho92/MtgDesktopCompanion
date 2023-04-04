0638b48e6465fff51e47770de22582cbefb41fd3
Nicho <nicolas.pihen@gmail.com>
2023-04-04 17:19:23 +0200

    Update JsonMessageRenderer.java

--- src/main/java/org/magic/gui/renderer/JsonMessageRenderer.java
+++ src/main/java/org/magic/gui/renderer/JsonMessageRenderer.java
@@ -1,7 +1,6 @@
 package org.magic.gui.renderer;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.util.Date;
 
@@ -10,9 +9,10 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.ListCellRenderer;
-import javax.swing.border.LineBorder;
+import javax.swing.SwingConstants;
 
 import org.magic.api.beans.JsonMessage;
 import org.magic.services.tools.ImageTools;
@@ -20,12 +20,12 @@ import org.magic.services.tools.UITools;
 
 public class JsonMessageRenderer implements ListCellRenderer<JsonMessage> {
 
-	private final int iconSize=50;
+	private final int iconSize=25;
 	
 	@Override
 	public Component getListCellRendererComponent(JList<? extends JsonMessage> list, JsonMessage value, int index,boolean isSelected, boolean cellHasFocus) {
 		var comp= component(value);
-		comp.setBorder(new LineBorder(Color.BLACK));
+		//comp.setBorder(new LineBorder(Color.BLACK));
 		return comp;
 	}
 	
@@ -34,11 +34,13 @@ public class JsonMessageRenderer implements ListCellRenderer<JsonMessage> {
 		
 		var panel = new JPanel();
 		panel.setLayout(new BorderLayout(0, 0));
-		panel.add(new JLabel(new ImageIcon(ImageTools.resize(value.getAuthor().getAvatar(), iconSize, iconSize))), BorderLayout.WEST);
 		
-		var lbl = new JLabel("<html><b>"+value.getAuthor().getName()+"</b><i> ("+UITools.formatDateTime(new Date(value.getTimeStamp()))+")</i></html>"  );
+		var lbl = new JLabel("<html><b>"+value.getAuthor().getName()+"</b><i> ("+UITools.formatDateTime(new Date(value.getTimeStamp()))+")</i></html>",
+							 new ImageIcon(ImageTools.resize(value.getAuthor().getAvatar(), iconSize, iconSize)),
+							 SwingConstants.LEFT  );
 		
 		panel.add(lbl, BorderLayout.NORTH);
+		panel.add(new JSeparator(SwingConstants.VERTICAL),BorderLayout.WEST);
 		
 		JTextArea textArea = new JTextArea(value.getMessage());
 		textArea.setEditable(false);
