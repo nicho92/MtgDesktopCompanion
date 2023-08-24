679a7d4fb7ef9e8da70118ae48a76bdf49aa815d
nicol <nicol@ANTEC.mshome.net>
2023-08-24 15:05:48 +0200

    update dork notifier

--- src/main/java/org/magic/api/notifiers/impl/DorkBox.java
+++ src/main/java/org/magic/api/notifiers/impl/DorkBox.java
@@ -8,19 +8,21 @@ import org.magic.api.beans.MTGNotification.FORMAT_NOTIFICATION;
 import org.magic.api.interfaces.abstracts.AbstractMTGNotifier;
 
 import dorkbox.notify.Notify;
+import dorkbox.notify.Theme;
 
 public class DorkBox extends AbstractMTGNotifier {
 
 	@Override
 	public void send(MTGNotification notification) throws IOException {
-		Notify n = Notify.create()
-	      .title(notification.getTitle())
-	      .text(notification.getMessage());
+		
+		Notify n = Notify.Companion.create()
+									.title(notification.getTitle())
+									.text(notification.getMessage());
 
 
 
 	    if(getBoolean("DARK"))
-	    	n=n.darkStyle();
+	    	n.setTheme(Theme.Companion.getDefaultDark());
 
 		switch (notification.getType())
 		{
@@ -46,7 +48,7 @@ public class DorkBox extends AbstractMTGNotifier {
 
 	@Override
 	public String getVersion() {
-		return Notify.getVersion();
+		return Notify.version;
 	}
 
 	@Override
