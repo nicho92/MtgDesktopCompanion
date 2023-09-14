26a79fc6a8f531b1894fc2468b65ef35e40946cd
Nicho <nicolas.pihen@gmail.com>
2023-09-14 13:55:10 +0200

    Add files via upload

---
+++ src/main/java/org/magic/api/beans/messages/SearchMessage.java
@@ -0,0 +1,15 @@
+package org.magic.api.beans.messages;
+
+import org.magic.api.beans.abstracts.AbstractMessage;
+import org.magic.game.model.Player;
+
+public class SearchMessage extends AbstractMessage {
+
+	private static final long serialVersionUID = 1L;
+
+	protected SearchMessage(Player p) {
+		super(p);
+		// TODO Auto-generated constructor stub
+	}
+
+}
