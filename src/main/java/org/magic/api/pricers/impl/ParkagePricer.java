4f14f0a82bd8896e97344f73cd125a7daa1cf73f
nicol <nicol@ANTEC>
2023-09-29 16:02:27 +0200

    parkage update

--- src/main/java/org/magic/api/pricers/impl/ParkagePricer.java
+++ src/main/java/org/magic/api/pricers/impl/ParkagePricer.java
@@ -20,41 +20,50 @@ public class ParkagePricer extends AbstractPricesProvider {
 	public String getName() {
 		return "Parkage";
 	}
+	
+	@Override
+	public String getVersion() {
+		return "2.0";
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		var mc = new MagicCard();
+		mc.setName("Liliana of the veil");
+		
+		new ParkagePricer().getLocalePrice(mc);
+	}
 
 	@Override
 	protected List<MagicPrice> getLocalePrice(MagicCard card) throws IOException {
 
 		ArrayList<MagicPrice> ret= new ArrayList<>();
 
-		URLTools.extractAsHtml(URL_BASE+"/en/search/?text="+URLTools.encode(card.getName())+"&page=1&products_per_page=50&page_view=3")
-		.select("table.table-condensed > tbody > tr")
-		.forEach(tr->{
-			try {
-				if(!tr.select("select").hasAttr("disabled"))
-				{
-					var mp = new MagicPrice();
-							mp.setCountry(Locale.FRANCE.getDisplayCountry(MTGControler.getInstance().getLocale()));
-							mp.setMagicCard(card);
-							mp.setCurrency("EUR");
-							mp.setSite(getName());
-							mp.setUrl(tr.select("td").first().select("a").attr("href"));
-							mp.setSeller(tr.select("td").get(2).text());
+		
+		var url = "https://search.luciole.tech/api/search?text="+URLTools.encode(card.getName())+"&namespace=en&_ref=2&title=undefined&description=undefined&list=true&count=true&limit=25&offset=0&preorder=&isnew=&lang=&with_quantity=false&isDiscount=false&user_hash=";
+		
+		
+		URLTools.extractAsJson(url).getAsJsonObject().get("list").getAsJsonArray().forEach(je->{
+				var jo = je.getAsJsonObject();
+			System.out.println(jo);
+			var mp = new MagicPrice();
+					mp.setCountry(Locale.FRANCE.getDisplayCountry(MTGControler.getInstance().getLocale()));
+					mp.setMagicCard(card);
+					mp.setCurrency("EUR");
+					mp.setSite(getName());
+					mp.setLanguage(jo.get("lang").getAsString());
+					mp.setFoil(jo.get("is_foil").getAsBoolean());
+					mp.setQty(jo.get("stock").getAsInt());
+					mp.setValue(jo.get("price").getAsDouble());
+					mp.setQuality(jo.get("state").getAsString());
+					mp.setUrl(URL_BASE+"/en/"+jo.get("id").getAsInt()+"-");
+					mp.setSeller(getName());
 
-							var urlFlag =tr.select("td").first().select("img").attr("src");
-							mp.setLanguage(urlFlag.substring(urlFlag.lastIndexOf('/')+1,urlFlag.lastIndexOf('.')));
-
-							var price = tr.select("td.col-price").text().replace(',', '.').replace("€","");
-							mp.setValue(Double.parseDouble(price));
-							mp.setFoil(!tr.select("td").first().select("i.fa-star").isEmpty());
-
-					ret.add(mp);
-				}
-			}catch(Exception e)
-			{
-				logger.error(e);
-			}
+					if(mp.getQty()>0)
+						ret.add(mp);
+	
 		});
-
+		
 		logger.info("{} found {} offers",getName(),ret.size());
 
 		return ret;
