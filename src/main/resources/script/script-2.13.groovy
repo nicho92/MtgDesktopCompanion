fa95241cecddb74d0b0e9af698fbcd9a837c3d75
nicol <nicol@host.docker.internal>
2023-10-09 20:39:54 +0200

    remove unsupported script

--- src/main/resources/script/script-2.13.groovy
+++
@@ -1,9 +0,0 @@
-if(dao.isSQL()) {
-	printf("Executing db update on " + dao.getName());
-	dao.executeQuery("ALTER TABLE stocks ADD grading "+dao.beanStorage());
-	printf("--done");
-}
-else
-{
-	printf("Your DAO is not SQL. Don't need to pass script");
-}
