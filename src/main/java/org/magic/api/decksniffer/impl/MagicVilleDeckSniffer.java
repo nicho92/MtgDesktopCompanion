8e87466683c96d6d2251d81ec567f367f3d7df03
nicol <nicol@ANTEC>
2024-01-02 21:22:30 +0100

    updates mv

--- src/main/java/org/magic/api/decksniffer/impl/MagicVilleDeckSniffer.java
+++ src/main/java/org/magic/api/decksniffer/impl/MagicVilleDeckSniffer.java
@@ -51,12 +51,7 @@ public class MagicVilleDeckSniffer extends AbstractDeckSniffer {
 		var doc = RequestBuilder.build().setClient(URLTools.newClient()).get().url(info.getUrl()).toHtml();
 		var urlimport = baseUrl+doc.select("div.lil_menu > a[href^=dl_appr]").first().attr("href");
 		var content = RequestBuilder.build().setClient(URLTools.newClient()).get().url(urlimport).toContentString();
-		var imp = new Apprentice2DeckExport() {
-			@Override
-			protected String getStringPattern() {
-				return "nocomma";
-			}
-		};
+		var imp = new Apprentice2DeckExport();
 
 		try {
 			imp.addObserver(listObservers().get(0));
