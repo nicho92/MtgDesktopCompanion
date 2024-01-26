d8c35fe7c4082f3b3c1a5685e34c044a96cf16a7
nicol <nicol@ANTEC>
2024-01-26 20:18:58 +0100

    booster for OJT

--- src/main/resources/script/utils/updateCollection.groovy
+++ src/main/resources/script/utils/updateCollection.groovy
@@ -5,18 +5,21 @@ import java.util.Date;
 import java.util.stream.Collectors;
 
 	var formater = new SimpleDateFormat("yyyy-MM-dd");
-	var dateBefore = formater.parse("2022-08-01");
+	var dateBefore = formater.parse("2024-01-24");
 	var col = new MTGCollection("Library");
 	
 
-String ed ="MIR";
-//dao.listEditionsIDFromCollection(col).each{ ed->
+//String ed ="MIR";
+dao.listEditionsIDFromCollection(col).each{ ed->
  	 System.out.println("========================================="+ed);
 	 dao.listCardsFromCollection(col, new MTGEdition(ed)).stream().filter(mc->mc.getDateUpdated().before(dateBefore)).collect(Collectors.toList()).each{ c->
 		try {
 			var newC = provider.getCardByScryfallId(c.getScryfallId());
 			if(newC!=null)
 			{
+				c.setMultiverseid(newC.getMultiverseid());
+				c.setNumber(newC.getNumber());
+				
 				dao.updateCard(c,newC, col);
 				System.out.println(""+col + ";" + newC.getEdition() + ";" + c + ";OK;" + newC+"\n");
 			}
@@ -28,5 +31,4 @@ String ed ="MIR";
 			System.out.println(""+col + ";" + newC.getEdition() + ";" + c + ";ERROR;" + e+"\n");
 		} 
 	 };
-	 
-//};
+};
