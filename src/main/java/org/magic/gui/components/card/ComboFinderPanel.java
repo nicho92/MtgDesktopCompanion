87a6bac3579fdde082481474a001740f7d4c688b
nicol <nicol@host.docker.internal>
2024-03-07 22:26:31 +0100

    source clean

--- src/main/java/org/magic/gui/components/card/ComboFinderPanel.java
+++ src/main/java/org/magic/gui/components/card/ComboFinderPanel.java
@@ -15,7 +15,6 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.SwingWorker;
 
