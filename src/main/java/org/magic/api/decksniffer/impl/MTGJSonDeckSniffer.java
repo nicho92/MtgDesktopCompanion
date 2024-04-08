2fc6f95e36deecb79d08e263362a0f75fb87236d
nicol <nicol@ANTEC>
2024-04-08 13:33:09 +0200

    fix MTGJson deck sniffer ( commander fix)

--- src/main/java/org/magic/api/decksniffer/impl/MTGJSonDeckSniffer.java
+++ src/main/java/org/magic/api/decksniffer/impl/MTGJSonDeckSniffer.java
@@ -28,12 +28,6 @@ public class MTGJSonDeckSniffer extends AbstractDeckSniffer {
 	@Override
 	public MTGDeck getDeck(RetrievableDeck info) throws IOException {
 		var el = URLTools.extractAsJson(info.getUrl().toString()).getAsJsonObject();
-		var mainBoard = el.get("data").getAsJsonObject().get("mainBoard").getAsJsonArray();
-		JsonArray sideBoard=null;
-
-		if(el.getAsJsonObject().get("sideBoard")!=null)
-			sideBoard = el.getAsJsonObject().get("sideBoard").getAsJsonArray();
-
 
 		MTGDeck deck = info.toBaseDeck();
 
@@ -43,38 +37,50 @@ public class MTGJSonDeckSniffer extends AbstractDeckSniffer {
 		{
 			//do nothing
 		}
-
-
-
-		init(deck,mainBoard,false);
-
-		if(sideBoard!=null)
-			init(deck,sideBoard,true);
-
+		
+		init(deck,el.get("data").getAsJsonObject().get("mainBoard").getAsJsonArray(),false,false);
+		
+		if(el.getAsJsonObject().get("sideBoard")!=null)
+			init(deck,el.getAsJsonObject().get("sideBoard").getAsJsonArray(),true,false);
+		
+		
+		if(el.get("data").getAsJsonObject().get("commander")!=null)
+			init(deck,el.get("data").getAsJsonObject().get("commander").getAsJsonArray(),false,true);
+		
+		
 		return deck;
 	}
 
 
-	private void init(MTGDeck d, JsonArray arr, boolean side)
+	private void init(MTGDeck d, JsonArray arr, boolean side, boolean commander)
 	{
 			arr.forEach(element->{
 
 			var ed = element.getAsJsonObject().get("printings").getAsJsonArray().get(0).getAsString();
 			var qty = element.getAsJsonObject().get("count").getAsInt();
-			var name = element.getAsJsonObject().get("name").getAsString();
+			var scryfallId = element.getAsJsonObject().get("identifiers").getAsJsonObject().get("scryfallId").getAsString();
+			var name = element.getAsJsonObject().get("name").getAsString();			
 			try {
-				MTGCard mc = getEnabledPlugin(MTGCardsProvider.class).searchCardByName(name, new MTGEdition(ed), true).get(0);
+				var mc = getEnabledPlugin(MTGCardsProvider.class).getCardByScryfallId(scryfallId);
 				if(!side)
 					d.getMain().put(mc, qty);
 				else
 					d.getSideBoard().put(mc, qty);
-
+				
 				notify(mc);
+				
+				  if(commander)
+					   d.setCommander(mc);
+				
 			} catch (Exception e) {
 				logger.error("error loading {}/{}",name,ed,e);
 			}
-
+			
+			
 		});
+			
+	 
+			
 	}
 
 
@@ -92,7 +98,7 @@ public class MTGJSonDeckSniffer extends AbstractDeckSniffer {
 							rd.setName(ob.get("name").getAsString());
 							rd.setAuthor("MtgJson");
 							try {
-								rd.setDescription(getEnabledPlugin(MTGCardsProvider.class).getSetById(ob.get("type").getAsString()).getSet());
+								rd.setDescription(getEnabledPlugin(MTGCardsProvider.class).getSetById(ob.get("code").getAsString()).getSet());
 								rd.setUrl(URI.create(AbstractMTGJsonProvider.MTG_JSON_DECKS+ob.get("fileName").getAsString()+".json"));
 							} catch (Exception e) {
 								logger.error(e);
