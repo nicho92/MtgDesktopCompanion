43bd5ca8916493d0d2c59d4ccc7b2e445ed19265
Noah Stuertz <noah@coru.sh>
2024-09-02 20:15:39 +0200

    add nix support

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Magic The Gathering: Desktop Companion";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    systems,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.callPackage ./package.nix {};
+    });
+}
