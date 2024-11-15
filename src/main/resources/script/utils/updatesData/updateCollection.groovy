c9dda88ba400eb8e9137df71d3686bcd6534882b
Nicho <nicolas.pihen@gmail.com>
2024-11-15 15:36:05 +0100

    Update updateCollection.groovy

--- src/main/resources/script/utils/updatesData/updateCollection.groovy
+++ src/main/resources/script/utils/updatesData/updateCollection.groovy
@@ -4,14 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-	var formater = new SimpleDateFormat("yyyy-MM-dd");
+	
+var formater = new SimpleDateFormat("yyyy-MM-dd");
 	//var dateBefore = formater.parse("2024-01-24");
 	var dateBefore = new Date();
 	var col = new MTGCollection("Library");
 	
 
-String ed ="10E";
-//dao.listEditionsIDFromCollection(col).each{ ed->
+
+dao.listEditionsIDFromCollection(col).each{ ed->
  	 System.out.println("========================================="+ed);
 	 dao.listStocks(col, new MTGEdition(ed)).stream().filter(mcs->mcs.getDateUpdate().before(dateBefore)).collect(Collectors.toList()).each{ mcs->
 		try {
@@ -22,4 +23,4 @@ String ed ="10E";
 			System.out.println(""+col + ";" + newC.getEdition() + ";" + c + ";ERROR;" + e+"\n");
 		} 
 	 };
-//};
+};
