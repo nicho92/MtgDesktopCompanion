d1ca70850e94a6ac76718e42948b2d99c5177423
nicol <nicol@ANTEC>
2025-04-14 08:58:03 +0200

    source clean

--- src/main/java/org/magic/gui/StockPanelGUI.java
+++ src/main/java/org/magic/gui/StockPanelGUI.java
@@ -8,7 +8,6 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.SystemColor;
 import java.awt.event.KeyEvent;
 import java.io.File;
