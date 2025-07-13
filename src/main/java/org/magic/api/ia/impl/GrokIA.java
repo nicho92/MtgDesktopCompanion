2b3bd379cf25b8ba1815a387c83ba43d92b322d3
nicol <nicol@host.docker.internal>
2025-07-13 10:31:06 +0200

    update new IA

---
+++ src/main/java/org/magic/api/ia/impl/GrokIA.java
@@ -0,0 +1,65 @@
+package org.magic.api.ia.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.magic.api.beans.technical.MTGProperty;
+import org.magic.api.interfaces.abstracts.AbstractIA;
+import org.magic.services.tools.POMReader;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class GrokIA extends AbstractIA {
+
+	private static final String API_KEY = "API_KEY";
+
+	@Override
+	public String getVersion() {
+		return POMReader.readVersionFromPom(OpenAiChatModel.class, "/META-INF/maven/dev.langchain4j/langchain4j-open-ai/pom.properties");
+	}
+
+	@Override
+	public Map<String, MTGProperty> getDefaultAttributes() {
+			var map = super.getDefaultAttributes();
+			map.put("TEMPERATURE", MTGProperty.newIntegerProperty("0.7", "You can think of temperature like randomness, with low value is being least random (or most deterministic) and max being most random (least deterministic)", 0, 1));
+			map.put("MAX_TOKEN", MTGProperty.newIntegerProperty("2000","Maximum size of the prompt",50,5000));
+			map.put("LOG", MTGProperty.newBooleanProperty(FALSE, "enable chat model logger"));
+			map.put("MODEL", new MTGProperty("grok-3-mini","choose langage model","grok-3","grok-3-mini","grok-3-fast","grok-3-mini-fast","grok-4-0709"));
+			return map;
+	}
+
+	@Override
+	public ChatModel getEngine(ResponseFormat format) {
+		var b=  OpenAiChatModel.builder() 
+				 .apiKey(getAuthenticator().get(API_KEY))
+				 .baseUrl("https://api.x.ai/v1")
+				 .modelName(getString("MODEL"))
+				 .maxTokens(getInt("MAX_TOKEN"))
+				 .logRequests(getBoolean("LOG"))
+				 .logResponses(getBoolean("LOG"))
+				 .temperature(getDouble("TEMPERATURE"));
+		
+				if(format!=null)
+					b.responseFormat(format.toString());
+			        
+				 
+				 
+				 
+				 return b.build();
+	}
+	
+	@Override
+	public String getName() {
+		return "Grok";
+	}
+
+	@Override
+	public List<String> listAuthenticationAttributes() {
+		return List.of(API_KEY);
+	}
+	
+
+	
+}
