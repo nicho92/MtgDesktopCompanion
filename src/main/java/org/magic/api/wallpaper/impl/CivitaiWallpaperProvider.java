81a620e6e724fee5ff3b53c6c9c942220ded5972
nicol <nicol@host.docker.internal>
2025-08-22 14:54:28 +0200

    update civitai

--- src/main/java/org/magic/api/wallpaper/impl/CivitaiWallpaperProvider.java
+++ src/main/java/org/magic/api/wallpaper/impl/CivitaiWallpaperProvider.java
@@ -30,21 +30,20 @@ public class CivitaiWallpaperProvider extends AbstractWallpaperProvider {
 		return "CivitAI";
 	}
 	
-	public static void main(String[] args) {
-		new CivitaiWallpaperProvider().search("lara croft");
-	}
-	
-
 	@Override
 	public List<MTGWallpaper> search(String search) {
-		
 		var ret = new ArrayList<MTGWallpaper>();
+		
+		var page=1;
+		
+		while(ret.size()<getInt(LIMIT))
+		{
 		
 		var obj = RequestBuilder.build().setClient(URLTools.newClient()).get().url(BASE_URL+"/api/v1/models")
 				.addHeader("Authorization", "Bearer "+getAuthenticator().get("API_KEY"))
 				.addHeader(URLTools.CONTENT_TYPE, URLTools.HEADER_JSON)
-				.addContent("limit", getString(LIMIT))
-				.addContent("query", search)
+				.addContent(getString("SEARCH_MODE"), search)
+				.addContent("page", String.valueOf(page++))
 				.addContent("nsfw", getString("MATURE")).toJson().getAsJsonObject();
 		
 		logger.debug("ret = {}", obj);
@@ -55,31 +54,51 @@ public class CivitaiWallpaperProvider extends AbstractWallpaperProvider {
 			 {
 				 for(var image : modelVersion.getAsJsonObject().get("images").getAsJsonArray())
 				 {
-					 if(image.getAsJsonObject().get("type").getAsString().equals("image")) {
+					 if(image.getAsJsonObject().get("type").getAsString().equals("image")) 
+					 {
 						 var wall= new MTGWallpaper();
 						 wall.setProvider(getName());
 						 wall.setMature(el.getAsJsonObject().get("nsfw").getAsBoolean());
 						 wall.setName(el.getAsJsonObject().get("name").getAsString());
+						 try {
 						 wall.setAuthor(el.getAsJsonObject().get("creator").getAsJsonObject().get("username").getAsString());
+						 }
+						 catch(Exception _)
+						 {
+							 //do nothign
+						 }
+						 wall.setPublishDate(Date.from(Instant.parse(modelVersion.getAsJsonObject().get("publishedAt").getAsString())));
 						 el.getAsJsonObject().get("tags").getAsJsonArray().forEach(t->wall.getTags().add(t.getAsString()));
 						 wall.setUrl(URI.create(image.getAsJsonObject().get("url").getAsString()));
 						 wall.setUrlThumb(wall.getUrl());
+						
 						 
-						 if(ret.size()<getInt(LIMIT))
+						 if(ret.size()>getInt(LIMIT))
+						 {
+							 logger.info("{} return {} results", getName(),ret.size());
+							 return ret;
+						 }
+						 else
+						 {
 							 ret.add(wall);
+						 }
 					 }
 				 }
 			 }
 		}
-		logger.info("{} return {} results", getName(),ret.size());
 		
+		}
+		
+		logger.info("{} return {} results", getName(),ret.size());
 		return ret;
 	}
 
 	@Override
 	public Map<String, MTGProperty> getDefaultAttributes() {
 		return Map.of("MATURE",MTGProperty.newBooleanProperty(FALSE, "set to true if you want to return mature content"),
-								LIMIT,MTGProperty.newIntegerProperty("100", "Max results to return", 1, 200));
+							  LIMIT,MTGProperty.newIntegerProperty("100", "Max results to return", 1, 200),
+							  "SEARCH_MODE",new MTGProperty("tag", "set search mode by tags or by query", "tag","query")
+								);
 	}
 	
 
