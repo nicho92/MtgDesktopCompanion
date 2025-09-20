be6afbffbd24db7bf2b008405fc7fc063d47d507
nicol <nicol@ANTEC.mshome.net>
2025-09-20 09:51:31 +0200

    update sets id for magicAlbum and magicstudio

--- src/main/java/org/magic/api/exports/impl/MagicAlbumExport.java
+++ src/main/java/org/magic/api/exports/impl/MagicAlbumExport.java
@@ -90,7 +90,7 @@ public class MagicAlbumExport extends AbstractFormattedFileCardExport {
 			var foilnumber = ( !m.group(7).isEmpty()) ? Integer.parseInt(m.group(7)):0;
 			var regularNumber = ( !m.group(6).isEmpty()) ? Integer.parseInt(m.group(6)):0;
 			var proxyNumber = ( !m.group(9).isEmpty()) ? Integer.parseInt(m.group(9)):0;
-			var setCode = aliases.getSetIdFor(this, new MTGEdition(m.group(1)));
+			var setCode = aliases.getReversedSetIdFor(this, m.group(1));
 			var lang=m.group(5);
 			var cardName = m.group(2).replace("’", "'").replace("│", " // ");
 
