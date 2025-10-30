1c6316bd313571ec6c7c68e87f823accfe9c34a3
Nicho <nicolas.pihen@gmail.com>
2025-10-30 14:04:40 +0100

    Update ManaPoolPricer.java

--- src/main/java/org/magic/api/pricers/impl/ManaPoolPricer.java
+++ src/main/java/org/magic/api/pricers/impl/ManaPoolPricer.java
@@ -10,7 +10,6 @@ import org.api.manapool.model.enums.EnumFinish;
 import org.api.manapool.services.ManaPoolAPIService;
 import org.magic.api.beans.MTGCard;
 import org.magic.api.beans.MTGPrice;
-import org.magic.api.beans.enums.EnumCondition;
 import org.magic.api.beans.technical.MTGProperty;
 import org.magic.api.interfaces.abstracts.AbstractPricesProvider;
 import org.magic.services.MTGControler;
@@ -21,7 +20,6 @@ public class ManaPoolPricer extends AbstractPricesProvider {
 	public String getName() {
 		return "ManaPool";
 	}
-
 	@Override
 	protected List<MTGPrice> getLocalePrice(MTGCard card) throws IOException {
 		
@@ -33,12 +31,15 @@ public class ManaPoolPricer extends AbstractPricesProvider {
 			mp.setCardData(card);
 			mp.setCurrency("USD");
 			mp.setFoil(pv.getFinishId()==EnumFinish.FO);
+			mp.setSeller(getName());
+			
 			mp.setSite(getName());
-			mp.setUrl(pv.getUrl()+"?referrer_code=MP-unt564tzvR24HgRRfJk5EU");
+			mp.setUrl(pv.getUrl()+"?referrer_code=MP-unt564tzvR24HgRRfJk5EU&conditions="+pv.getConditionId().name()+ (mp.isFoil()?"&finish=foil":""));
+			mp.setSellerUrl(mp.getUrl());
 			mp.setValue(pv.getPrice());
 			mp.setLanguage(pv.getLanguageId().getLabel());
 			mp.setCountry(Locale.US.getDisplayCountry(MTGControler.getInstance().getLocale()));
-			mp.setQuality(aliases.getReversedConditionFor(this, pv.getConditionId().name(),EnumCondition.NEAR_MINT));
+			mp.setQuality(aliases.getReversedConditionFor(this, pv.getConditionId().name(),org.magic.api.beans.enums.EnumCondition.NEAR_MINT));
 			mp.setQty(pv.getAvailable());
 			ret.add(mp);
 		});
