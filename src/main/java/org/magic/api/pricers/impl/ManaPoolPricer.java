2b01d8e8b8499a797c73152f159ca8ab13fab107
Nicho <nicolas.pihen@gmail.com>
2025-10-30 11:45:43 +0100

    Replace price retrieval with ManaPoolAPIService
    Refactor getLocalePrice method to use ManaPoolAPIService for price retrieval instead of MTGJsonPricerProvider.

--- src/main/java/org/magic/api/pricers/impl/ManaPoolPricer.java
+++ src/main/java/org/magic/api/pricers/impl/ManaPoolPricer.java
@@ -1,15 +1,19 @@
 package org.magic.api.pricers.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import org.api.manapool.model.enums.EnumFinish;
+import org.api.manapool.services.ManaPoolAPIService;
 import org.magic.api.beans.MTGCard;
 import org.magic.api.beans.MTGPrice;
+import org.magic.api.beans.enums.EnumCondition;
 import org.magic.api.beans.technical.MTGProperty;
 import org.magic.api.interfaces.abstracts.AbstractPricesProvider;
-import org.magic.services.providers.MTGJsonPricerProvider;
-import org.magic.services.providers.MTGJsonPricerProvider.VENDOR;
+import org.magic.services.MTGControler;
 
 public class ManaPoolPricer extends AbstractPricesProvider {
 
@@ -20,17 +24,26 @@ public class ManaPoolPricer extends AbstractPricesProvider {
 
 	@Override
 	protected List<MTGPrice> getLocalePrice(MTGCard card) throws IOException {
-
-		logger.debug("{} looking for prices for {} ",getName(),card);
-
-		MTGJsonPricerProvider.getInstance().expirationDay(getInt("EXPIRE_FILE_DAYS"));
 		
+		var poolservice = new ManaPoolAPIService(getAuthenticator().get("EMAIL"),getAuthenticator().get("TOKEN"));
+		var ret = new ArrayList<MTGPrice>();
 		
-		return MTGJsonPricerProvider.getInstance().getPriceFor(card,VENDOR.MANAPOOL).stream().map(mp->{
-			mp.setUrl("https://manapool.com/");
-			return mp;
-			
-		}).toList();
+		poolservice.listVariantsPrices().stream().filter(pv->pv.getScryfallId().equals(card.getScryfallId()) && pv.getAvailable()>0).forEach(pv->{
+			var mp = new MTGPrice();
+			mp.setCardData(card);
+			mp.setCurrency("USD");
+			mp.setFoil(pv.getFinishId()==EnumFinish.FO);
+			mp.setSite(getName());
+			mp.setUrl(pv.getUrl()+"?referrer_code=MP-unt564tzvR24HgRRfJk5EU");
+			mp.setValue(pv.getPrice());
+			mp.setLanguage(pv.getLanguageId().getLabel());
+			mp.setCountry(Locale.US.getDisplayCountry(MTGControler.getInstance().getLocale()));
+			mp.setQuality(aliases.getReversedConditionFor(this, pv.getConditionId().name(),EnumCondition.NEAR_MINT));
+			mp.setQty(pv.getAvailable());
+			ret.add(mp);
+		});
+		
+		return ret;
 	}
 
 	@Override
