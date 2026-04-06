5c02b4c56c7d3a75a223aedc9b3a23d645dc5e1e
nicol <nicol@ANTEC>
2026-04-06 12:39:02 +0200

    cardnexus

--- src/main/java/org/magic/api/exports/impl/CardNexusExport.java
+++ src/main/java/org/magic/api/exports/impl/CardNexusExport.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.util.List;
 
 import org.magic.api.beans.MTGCardStock;
+import org.magic.api.beans.enums.EnumExportCategory;
 import org.magic.api.interfaces.abstracts.extra.AbstractFormattedFileCardExport;
 import org.magic.services.tools.FileTools;
+import org.magic.services.tools.UITools;
 
 public class CardNexusExport  extends AbstractFormattedFileCardExport {
 
@@ -21,6 +23,12 @@ public class CardNexusExport  extends AbstractFormattedFileCardExport {
 
 	
 	@Override
+	public EnumExportCategory getCategory() {
+		return EnumExportCategory.EXTERNAL_FILE_FORMAT;
+	}
+	
+	
+	@Override
 	public void exportStock(List<MTGCardStock> stock, File f) throws IOException {
 		
 		var builder = new StringBuilder(COLUMNS).append(System.lineSeparator());
@@ -28,7 +36,18 @@ public class CardNexusExport  extends AbstractFormattedFileCardExport {
 		
 		for(var mcs : stock)
 		{
+			builder.append("\"").append(mcs.getQte()).append("\"").append(getSeparator());
+			builder.append("\"").append(mcs.getProduct().getName()).append("\"").append(getSeparator());
+			builder.append("\"").append(mcs.getProduct().getNumber()).append("\"").append(getSeparator());
+			builder.append("\"").append(mcs.isFoil()?"Foil":"Standard").append("\"").append(getSeparator());
+			builder.append("\"").append("").append("\"").append(getSeparator());
+			builder.append("\"").append(mcs.getProduct().getEdition().getSet()).append("\"").append(getSeparator());
+			builder.append("\"").append("Magic: The Gathering").append("\"").append(getSeparator());
+			builder.append("\"").append(aliases.getConditionFor(this, mcs.getCondition())).append("\"").append(getSeparator());
+			builder.append("\"").append(mcs.getLanguage().substring(0, 2)).append("\"").append(getSeparator());
+			builder.append("\"").append(UITools.formatDouble(mcs.getPrice(),'.')).append("\"").append(System.lineSeparator());
 			
+			notify(mcs.getProduct());
 		}
 		
 		
