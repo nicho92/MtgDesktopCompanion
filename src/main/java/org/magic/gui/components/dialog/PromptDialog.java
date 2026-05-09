f1549a659271a2cf6e6bb1fcd353de86be86b708
nicol <nicol@ANTEC>
2026-05-09 12:02:59 +0200

    prompt dialog test

--- src/main/java/org/magic/gui/components/dialog/PromptDialog.java
+++ src/main/java/org/magic/gui/components/dialog/PromptDialog.java
@@ -4,13 +4,11 @@ import static org.magic.services.tools.MTG.capitalize;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
-
 import org.magic.services.MTGConstants;
 public class PromptDialog extends JDialog {
 
@@ -28,21 +26,20 @@ public class PromptDialog extends JDialog {
 		setIconImage(MTGConstants.ICON_TAB_IA.getImage());
 		setModal(true);
 		setLocationRelativeTo(null);
-		
-		
+
 		var panel = new JPanel();
 		editorPane = new JTextPane();
 		var btnImport = new JButton(MTGConstants.ICON_SAVE);
 		var btnCancel = new JButton(MTGConstants.ICON_CANCEL);
-		
+
 		btnImport.setToolTipText(capitalize("IMPORT"));
 		btnCancel.setToolTipText(capitalize("CANCEL"));
-		
+
 		getContentPane().add(panel, BorderLayout.SOUTH);
 		panel.add(btnImport);
 		panel.add(btnCancel);
 		getContentPane().add(new JScrollPane(editorPane), BorderLayout.CENTER);
-		
+
 		btnCancel.addActionListener(_ -> {
 			editorPane.setText("");
 			dispose();
