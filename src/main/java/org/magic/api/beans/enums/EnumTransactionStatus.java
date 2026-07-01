b0f3f1e4624a610b84f0e43a9adfccc94792e395
nicol <nicol@host.docker.internal>
2026-07-01 09:24:03 +0200

    updates

--- src/main/java/org/magic/api/beans/enums/EnumTransactionStatus.java
+++ src/main/java/org/magic/api/beans/enums/EnumTransactionStatus.java
@@ -6,12 +6,19 @@ import org.magic.api.interfaces.extra.MTGIconable;
 import org.magic.services.tools.UITools;
 
 public enum EnumTransactionStatus implements MTGIconable {
-	NEW(Color.GREEN, "New"), IN_PROGRESS(Color.ORANGE, "In Progress"), PAYMENT_WAITING(Color.YELLOW,
-			"Waiting for payment"), PAYMENT_SENT(Color.YELLOW, "Payment sent"), REFUSED(Color.RED,
-					"Refused"), PAID(Color.YELLOW, "Paid"), SENT(new Color(102, 178, 255), "Sent"), CLOSED(Color.BLACK,
-							"Closed"), CANCELED(Color.GRAY, "Canceled"), CANCELATION_ASK(new Color(192, 192, 192),
-									"Ask for Cancelation"), PRE_ORDERED(new Color(178, 102, 255),
-											"Pre Ordered"), DELIVRED(Color.YELLOW, "Delivred");
+	
+	NEW(Color.GREEN, "New"), 
+	IN_PROGRESS(Color.ORANGE, "In Progress"), 
+	PAYMENT_WAITING(Color.YELLOW, "Waiting for payment"), 
+	PAYMENT_SENT(Color.YELLOW, "Payment sent"), 
+	REFUSED(Color.RED,	"Refused"), 
+	PAID(Color.YELLOW, "Paid"), 
+	SENT(new Color(102, 178, 255), "Sent"), 
+	CLOSED(Color.BLACK,"Closed"), 
+	CANCELED(Color.GRAY, "Canceled"), 
+	CANCELATION_ASK(new Color(192, 192, 192),"Ask for Cancelation"), 
+	PRE_ORDERED(new Color(178, 102, 255),"Pre Ordered"), 
+	DELIVRED(Color.YELLOW, "Delivred");
 
 	private String name;
 	private Color color;
